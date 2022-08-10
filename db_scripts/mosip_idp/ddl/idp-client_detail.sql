d0843a65699b9a702425704a367e9a8cdac6d6e5
ase-101 <>
2022-08-11 00:24:08 +0530

    Added IDP DDL script

--- db_scripts/mosip_idp/ddl/idp-client_detail.sql
+++ db_scripts/mosip_idp/ddl/idp-client_detail.sql
@@ -14,17 +14,17 @@
 -- DROP TABLE IF EXISTS idp.client_detail CASCADE;
 CREATE TABLE idp.client_detail(
 	id character varying(50) NOT NULL,
-	name character varying(128) NOT NULL,
+	name character varying(256) NOT NULL,
+	rp_id character varying(50) NOT NULL,
 	logo_uri character varying(256) NOT NULL,
 	redirect_uris character varying NOT NULL,
-	rp_id character varying(50) NOT NULL,
-	status character varying(20) NOT NULL,
 	claims character varying NOT NULL,
 	amr_values character varying NOT NULL,
 	cert_data character varying NOT NULL,
 	cert_thumbprint character varying(100) NOT NULL,
-	CONSTRAINT pk_client_detail PRIMARY KEY (id)
-	CONSTRAINT uk_client_cert UNIQUE KEY (cert_thumbprint)
+	status character varying(20) NOT NULL,
+	CONSTRAINT pk_client_detail PRIMARY KEY (id),
+	CONSTRAINT uk_client_cert UNIQUE (cert_thumbprint)
 );
 -- ddl-end --
 COMMENT ON TABLE idp.id IS 'Client ID: Unique id assigned to registered OIDC client.';
