56f009e82a53daea5148f0e4870a54187f3a9b29
M1022006 <sowmya.goudar@mindtree.com>
2022-11-21 17:43:29 +0530

    MOSIP-24812 set up service

---
+++ idp-binding-service/src/test/java/io/mosip/idp/binding/IdpBindingServiceApplicationTest.java
@@ -0,0 +1,10 @@
+package io.mosip.idp.binding;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class IdpBindingServiceApplicationTest {
+	@Test
+	public void contextLoads() {}
+}
