e42959f5dfc32ce948f2c9b053f31aaaff06cba5
M1044292 <M1044292@C1ML54597.mindtree.com>
2022-11-23 13:04:00 +0530

    MOSIP-24757: Created db scripts

--- db_scripts/mosip_idpbinding/dml.sql
+++ db_scripts/mosip_idpbinding/dml.sql
@@ -3,7 +3,7 @@
 ----- TRUNCATE idpbinding.public_key_registry TABLE Data and It's reference Data and COPY Data from CSV file -----
 TRUNCATE TABLE idpbinding.public_key_registry cascade ;
 
--- \COPY idp.key_policy_def (APP_ID,KEY_VALIDITY_DURATION,PRE_EXPIRE_DAYS,ACCESS_ALLOWED,IS_ACTIVE,CR_BY,CR_DTIMES) FROM './dml/idp-key_policy_def.csv' delimiter ',' HEADER  csv;
+\COPY idpbinding.key_policy_def (APP_ID,KEY_VALIDITY_DURATION,PRE_EXPIRE_DAYS,ACCESS_ALLOWED,IS_ACTIVE,CR_BY,CR_DTIMES) FROM './dml/idpbinding-key_policy_def.csv' delimiter ',' HEADER  csv;
 
 
 
