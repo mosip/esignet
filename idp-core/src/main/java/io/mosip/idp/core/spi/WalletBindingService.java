8e855be241d47bf2265d9fcdc8ef1364a7c60cad
ase-101 <>
2022-11-29 00:39:22 +0530

    Added config files and caching methods

---
+++ idp-core/src/main/java/io/mosip/idp/core/spi/WalletBindingService.java
@@ -0,0 +1,17 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+package io.mosip.idp.core.spi;
+
+import io.mosip.idp.core.exception.IdPException;
+
+public interface WalletBindingService {
+
+    void sendBindingOtp() throws IdPException;
+
+    void bindWallet() throws IdPException;
+
+    void validateBinding() throws IdPException;
+}
