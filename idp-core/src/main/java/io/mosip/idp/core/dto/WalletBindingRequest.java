da442dc1fb5d0043edb693c324235086dc853c02
M1022006 <sowmya.goudar@mindtree.com>
2022-11-30 13:10:26 +0530

    MOSIP-24789 intial changes

---
+++ idp-core/src/main/java/io/mosip/idp/core/dto/WalletBindingRequest.java
@@ -0,0 +1,37 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+package io.mosip.idp.core.dto;
+
+
+import java.util.Map;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+
+import io.mosip.idp.core.util.ErrorConstants;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class WalletBindingRequest {
+	
+	@NotBlank(message = ErrorConstants.INVALID_TRANSACTION_ID)
+    private String transactionId;
+
+    @NotBlank(message = ErrorConstants.INVALID_IDENTIFIER)
+    private String individualId;
+
+
+    private AuthChallenge authChallenge;
+    
+    @NotEmpty(message = ErrorConstants.INVALID_PUBLIC_KEY)
+    private Map<String, Object> publicKey;
+}
