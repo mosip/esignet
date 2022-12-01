0ef7e554ca53733c70052eb34eef4934d71586fb
M1022006 <sowmya.goudar@mindtree.com>
2022-12-01 16:07:21 +0530

    MOSIP-24789 Added controller test case

--- idp-binding-service/src/test/java/io/mosip/idp/binding/controllers/WalletBindingControllerTest.java
+++ idp-binding-service/src/test/java/io/mosip/idp/binding/controllers/WalletBindingControllerTest.java
@@ -12,14 +12,15 @@ import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nimbusds.jose.jwk.JWK;
@@ -31,46 +32,49 @@ import io.mosip.idp.core.dto.WalletBindingRequest;
 import io.mosip.idp.core.dto.WalletBindingResponse;
 import io.mosip.idp.core.spi.WalletBindingService;
 
-
-
-@RunWith(SpringRunner.class)
-@WebMvcTest(value = WalletBindingController.class)
+@RunWith(MockitoJUnitRunner.class)
 public class WalletBindingControllerTest {
-	
-    @Autowired
-    MockMvc mockMvc;
 
-	@MockBean
-    private WalletBindingService walletBindingService;
-	
-	 ObjectMapper objectMapper = new ObjectMapper();
-	
-	private JWK clientJWK =TestUtil.generateJWK_RSA();
-	
-	    @Test
-	    public void bindWallet_returnSuccessResponse() throws Exception {
-	    	WalletBindingRequest walletBindingRequest=new WalletBindingRequest();
-	    	walletBindingRequest.setIndividualId("id1");
-	    	walletBindingRequest.setPublicKey((Map<String, Object>) objectMapper.readValue(clientJWK.toJSONString(),HashMap.class));
-	    	
-	    	walletBindingRequest.setTransactionId("123456789");
-	    	AuthChallenge authChallenge=new AuthChallenge();
-	    	authChallenge.setAuthFactorType("OTP");
-	    	authChallenge.setChallenge("12345");
-	    	walletBindingRequest.setAuthChallenge(authChallenge);
-	    	  ZonedDateTime requestTime = ZonedDateTime.now(ZoneOffset.UTC);
-	    	 RequestWrapper wrapper = new RequestWrapper<>();
-	         wrapper.setRequestTime(requestTime.format(DateTimeFormatter.ofPattern(UTC_DATETIME_PATTERN)));
-	         wrapper.setRequest(walletBindingRequest);
+	MockMvc mockMvc;
 
-	         WalletBindingResponse walletBindingResponse = new WalletBindingResponse();
-	         walletBindingResponse.setTransactionId("123456789");
-	         when(walletBindingService.bindWallet(walletBindingRequest)).thenReturn(walletBindingResponse);
+	@Mock
+	private WalletBindingService walletBindingService;
 
-	         mockMvc.perform(post("/wallet-binding")
-	                         .content(objectMapper.writeValueAsString(wrapper))
-	                         .contentType(MediaType.APPLICATION_JSON))
-	                 .andExpect(status().isOk())
-	                 .andExpect(jsonPath("$.response.transactionId").value("123456789"));
-	 }
+	ObjectMapper objectMapper = new ObjectMapper();
+
+	private JWK clientJWK = TestUtil.generateJWK_RSA();
+
+	@InjectMocks
+	WalletBindingController walletBindingController;
+
+	@Before
+	public void setup() {
+		mockMvc = MockMvcBuilders.standaloneSetup(this.walletBindingController).build();
+	}
+
+	@Test
+	public void bindWallet_returnSuccessResponse() throws Exception {
+		WalletBindingRequest walletBindingRequest = new WalletBindingRequest();
+		walletBindingRequest.setIndividualId("id1");
+		walletBindingRequest
+				.setPublicKey((Map<String, Object>) objectMapper.readValue(clientJWK.toJSONString(), HashMap.class));
+
+		walletBindingRequest.setTransactionId("123456789");
+		AuthChallenge authChallenge = new AuthChallenge();
+		authChallenge.setAuthFactorType("OTP");
+		authChallenge.setChallenge("12345");
+		walletBindingRequest.setAuthChallenge(authChallenge);
+		ZonedDateTime requestTime = ZonedDateTime.now(ZoneOffset.UTC);
+		RequestWrapper wrapper = new RequestWrapper<>();
+		wrapper.setRequestTime(requestTime.format(DateTimeFormatter.ofPattern(UTC_DATETIME_PATTERN)));
+		wrapper.setRequest(walletBindingRequest);
+
+		WalletBindingResponse walletBindingResponse = new WalletBindingResponse();
+		walletBindingResponse.setTransactionId("123456789");
+		when(walletBindingService.bindWallet(walletBindingRequest)).thenReturn(walletBindingResponse);
+
+		mockMvc.perform(post("/wallet-binding").content(objectMapper.writeValueAsString(wrapper))
+				.contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+				.andExpect(jsonPath("$.response.transactionId").value("123456789"));
+	}
 }
