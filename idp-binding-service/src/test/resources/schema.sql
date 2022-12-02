72cf74bac5f9ec2c18d4980f6da6f3f3892d62fb
M1022006 <sowmya.goudar@mindtree.com>
2022-12-02 13:13:09 +0530

    MOSIP-24789 code changes

--- idp-binding-service/src/test/resources/schema.sql
+++ idp-binding-service/src/test/resources/schema.sql
@@ -12,3 +12,49 @@ CREATE TABLE id_token_mapping (
 	psu_token character varying(256) NOT NULL,
 	CONSTRAINT pk_id_token_mapping PRIMARY KEY (id_hash)
 );
+CREATE TABLE key_alias(
+    id character varying(36) NOT NULL,
+    app_id character varying(36) NOT NULL,
+    ref_id character varying(128),
+    key_gen_dtimes timestamp,
+    key_expire_dtimes timestamp,
+    status_code character varying(36),
+    lang_code character varying(3),
+    cr_by character varying(256) NOT NULL,
+    cr_dtimes timestamp NOT NULL,
+    upd_by character varying(256),
+    upd_dtimes timestamp,
+    is_deleted boolean DEFAULT FALSE,
+    del_dtimes timestamp,
+    cert_thumbprint character varying(100),
+    uni_ident character varying(50),
+    CONSTRAINT pk_keymals_id PRIMARY KEY (id),
+    CONSTRAINT uni_ident_const UNIQUE (uni_ident)
+);
+CREATE TABLE key_policy_def(
+    app_id character varying(36) NOT NULL,
+    key_validity_duration smallint,
+    is_active boolean NOT NULL,
+    pre_expire_days smallint,
+    access_allowed character varying(1024),
+    cr_by character varying(256) NOT NULL,
+    cr_dtimes timestamp NOT NULL,
+    upd_by character varying(256),
+    upd_dtimes timestamp,
+    is_deleted boolean DEFAULT FALSE,
+    del_dtimes timestamp,
+    CONSTRAINT pk_keypdef_id PRIMARY KEY (app_id)
+);
+CREATE TABLE key_store(
+	id character varying(36) NOT NULL,
+	master_key character varying(36) NOT NULL,
+	private_key character varying(2500) NOT NULL,
+	certificate_data character varying NOT NULL,
+	cr_by character varying(256) NOT NULL,
+	cr_dtimes timestamp NOT NULL,
+	upd_by character varying(256),
+	upd_dtimes timestamp,
+	is_deleted boolean DEFAULT FALSE,
+	del_dtimes timestamp,
+	CONSTRAINT pk_keystr_id PRIMARY KEY (id)
+);
