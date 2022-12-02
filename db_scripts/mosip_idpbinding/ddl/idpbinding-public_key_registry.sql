e1bf135aeee33a44b19c503b4171aa7f7594489d
M1022006 <sowmya.goudar@mindtree.com>
2022-12-02 19:09:28 +0530

    MOSIP-24757code changes
    Signed-off-by: ase-101 <sunkadaeanusha@gmail.com>

--- db_scripts/mosip_idpbinding/ddl/idpbinding-public_key_registry.sql
+++ db_scripts/mosip_idpbinding/ddl/idpbinding-public_key_registry.sql
@@ -18,7 +18,7 @@
 CREATE TABLE public_key_registry(
     id_hash character varying(50) NOT NULL,
 	psu_token character varying(256) NOT NULL,
-	public_key character varying(50) NOT NULL,
+	public_key character varying NOT NULL,
 	expire_dtimes timestamp NOT NULL,
 	wallet_binding_id character varying(256) NOT NULL,
 	public_key_hash character varying(50) NOT NULL,
