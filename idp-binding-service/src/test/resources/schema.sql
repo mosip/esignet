d2432f7f44b58690e334300127b807b921912bc3
M1022006 <sowmya.goudar@mindtree.com>
2022-12-02 18:53:35 +0530

    MOSIP-24757code changes

--- idp-binding-service/src/test/resources/schema.sql
+++ idp-binding-service/src/test/resources/schema.sql
@@ -1,14 +1,10 @@
 CREATE TABLE public_key_registry(
+    id_hash character varying(50) NOT NULL,
 	psu_token character varying(256) NOT NULL,
 	public_key character varying(50) NOT NULL,
 	expire_dtimes timestamp NOT NULL,
 	wallet_binding_id character varying(256) NOT NULL,
+	public_key_hash character varying(50) NOT NULL,
 	cr_dtimes timestamp NOT NULL,
-	CONSTRAINT pk_public_key_registry PRIMARY KEY (psu_token),
-	CONSTRAINT uk_public_key_registry UNIQUE (public_key)
-);
-CREATE TABLE id_token_mapping (
-	id_hash character varying(50) NOT NULL,
-	psu_token character varying(256) NOT NULL,
-	CONSTRAINT pk_id_token_mapping PRIMARY KEY (id_hash)
+	CONSTRAINT pk_public_key_registry PRIMARY KEY (id_hash)
 );
