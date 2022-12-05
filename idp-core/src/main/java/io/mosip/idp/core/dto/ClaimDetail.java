4d45c1fff2f9acb268b4bed5003b33c5bf359d09
ase-101 <sunkadaeanusha@gmail.com>
2022-12-05 17:31:15 +0530

    Update ClaimDetail.java
    Signed-off-by: ase-101 <sunkadaeanusha@gmail.com>

--- idp-core/src/main/java/io/mosip/idp/core/dto/ClaimDetail.java
+++ idp-core/src/main/java/io/mosip/idp/core/dto/ClaimDetail.java
@@ -10,11 +10,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class ClaimDetail {
+public class ClaimDetail implements Serializable {
 
     private String value;
     private String[] values;
