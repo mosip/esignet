13e011639b3bf0e9253ac95a14019782917e4329
M1022006 <sowmya.goudar@mindtree.com>
2022-12-06 13:51:10 +0530

    MOSIP-24789 review comment fix

--- idp-binding-service/src/main/java/io/mosip/idp/binding/advice/IdpBindingAuthenticationEntryPoint.java
+++ idp-binding-service/src/main/java/io/mosip/idp/binding/advice/IdpBindingAuthenticationEntryPoint.java
@@ -1,15 +1,21 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
 package io.mosip.idp.binding.advice;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerExceptionResolver;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Component
 public class IdpBindingAuthenticationEntryPoint implements AuthenticationEntryPoint {
