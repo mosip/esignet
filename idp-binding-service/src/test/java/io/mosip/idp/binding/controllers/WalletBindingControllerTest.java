9e6b443ab31aa85b203fca29074f6894bdd73457
M1022006 <sowmya.goudar@mindtree.com>
2022-12-06 13:51:10 +0530

    MOSIP-24789 review comment fix
    Signed-off-by: ase-101 <sunkadaeanusha@gmail.com>

--- idp-binding-service/src/test/java/io/mosip/idp/binding/controllers/WalletBindingControllerTest.java
+++ idp-binding-service/src/test/java/io/mosip/idp/binding/controllers/WalletBindingControllerTest.java
@@ -1,3 +1,8 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
 package io.mosip.idp.binding.controllers;
 
 import static io.mosip.idp.core.util.Constants.UTC_DATETIME_PATTERN;
