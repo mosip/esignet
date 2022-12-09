caa2bd29bc83d3160d0c607c8ae1d01c6e57bec0
ckm007 <chandrakeshavmishra@gmail.com>
2022-12-09 16:05:27 +0530

    [MOSIP-1752] added *-all shell scrpts

---
+++ helm/delete-all.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# deletes Idp services in correct order
+## Usage: ./delete-all.sh [kubeconfig]
+
+if [ $# -ge 1 ] ; then
+  export KUBECONFIG=$1
+fi
+
+ROOT_DIR=`pwd`
+
+declare -a module=("redis"
+                   "idp"
+                   "idp-binding"
+                   )
+
+echo Installing IDP services
+
+for i in "${module[@]}"
+do
+  cd $ROOT_DIR/"$i"
+  ./delete.sh
+done
+
+echo All IDP services deleted sucessfully.
