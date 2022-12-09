56975c125bebfe6dd4f6d64bb2f0232679fd3605
ckm007 <chandrakeshavmishra@gmail.com>
2022-12-09 16:05:27 +0530

    [MOSIP-1752] added *-all shell scrpts
    Signed-off-by: ase-101 <sunkadaeanusha@gmail.com>

---
+++ helm/restart-all.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# restarts Idp services in correct order
+## Usage: ./restart-all.sh [kubeconfig]
+
+if [ $# -ge 1 ] ; then
+  export KUBECONFIG=$1
+fi
+
+ROOT_DIR=`pwd`
+
+declare -a module=("redis"
+                   "idp"
+                   "idp-binding"
+                   )
+
+echo restarting IDP services
+
+for i in "${module[@]}"
+do
+  cd $ROOT_DIR/"$i"
+  ./restart.sh
+done
+
+echo All IDP services restarted sucessfully.
