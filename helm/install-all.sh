794e4b5234f7dea17518855bd03fbe0ef6c899e8
ckm007 <chandrakeshavmishra@gmail.com>
2022-12-16 18:03:28 +0530

    [DSD-1752] added changes for idp movement

--- helm/install-all.sh
+++ helm/install-all.sh
@@ -9,6 +9,12 @@ fi
 
 ROOT_DIR=`pwd`
 
+CHART_VERSION=12.0.1-B2
+echo Installing Softhsm for IDA
+helm -n $NS install softhsm-ida mosip/softhsm -f softhsm-values.yaml --version $CHART_VERSION --wait
+echo Installed Softhsm for IDA
+
+
 declare -a module=("redis"
                    "idp"
                    "idp-binding"
