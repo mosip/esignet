8a95f0c6b711c3e16afef0f4217efd393204ef14
ase-101 <>
2022-12-27 08:16:19 +0530

    MOSIP-25347
    Signed-off-by: ase-101 <sunkadaeanusha@gmail.com>

--- idp-service/src/main/java/io/mosip/idp/controllers/CsrfController.java
+++ idp-service/src/main/java/io/mosip/idp/controllers/CsrfController.java
@@ -1,5 +1,9 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
 package io.mosip.idp.controllers;
-
 
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
