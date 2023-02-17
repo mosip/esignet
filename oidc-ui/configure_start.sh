4e36a97fdf9943cf5aa729940aa64fcbdcf252a9
anshulv1401 <anshulv1401@gmail.com>
2023-02-17 15:04:51 +0530

    artifcatory repo url updated

--- oidc-ui/configure_start.sh
+++ oidc-ui/configure_start.sh
@@ -4,13 +4,13 @@
 set -e
 
 echo "Downloading pre-requisites install scripts"
-wget --no-check-certificate --no-cache --no-cookies $artifactory_url_env/artifactory/libs-release-local/i18n/idp-i18n-bundle.zip -O $i18n_path/idp-i18n-bundle.zip
+wget --no-check-certificate --no-cache --no-cookies $artifactory_url_env/artifactory/libs-release-local/i18n/esignet-i18n-bundle.zip -O $i18n_path/esignet-i18n-bundle.zip
 
 echo "unzip pre-requisites.."
 chmod 775 $i18n_path/*
 
 cd $i18n_path
-unzip -o idp-i18n-bundle.zip
+unzip -o esignet-i18n-bundle.zip
 
 echo "unzip pre-requisites completed."
 
