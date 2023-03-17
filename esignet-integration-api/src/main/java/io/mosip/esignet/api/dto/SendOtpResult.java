472b9abb5d2b495910bbca4e6fd945ee6fc9dc27
anshulv1401 <31562315+anshulv1401@users.noreply.github.com>
2023-03-17 21:56:31 +0530

    MOSIP-26509 error messages for mock and ida added (#160)
    * MOSIP-26509 error messages for mock and ida added
    
    * MOSIP-26509 impl of sendOTP added to mock IDA

--- esignet-integration-api/src/main/java/io/mosip/esignet/api/dto/SendOtpResult.java
+++ esignet-integration-api/src/main/java/io/mosip/esignet/api/dto/SendOtpResult.java
@@ -7,9 +7,11 @@ package io.mosip.esignet.api.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class SendOtpResult {
 
     private String transactionId;
