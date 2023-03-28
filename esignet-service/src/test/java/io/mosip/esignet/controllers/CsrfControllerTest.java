23862c3901baaa7cf7e3048d2109709380ded4d0
Himaja Dhanyamraju <43470317+HimajaDhanyamraju2@users.noreply.github.com>
2023-03-28 20:58:33 +0530

    Added test cases (#170)
    * MOSIP-26295: Auditing changes
    
    * Modified as per review comments
    
    * Modified auditing
    
    * Modified comments
    
    * Renamed IdPException to EsignetException
    
    * Fixed test cases
    
    * Fixed sonar bugs
    
    * Added test cases
    
    ---------
    
    Co-authored-by: M1044292 <M1044292@C1ML54597.mindtree.com>

---
+++ esignet-service/src/test/java/io/mosip/esignet/controllers/CsrfControllerTest.java
@@ -0,0 +1,41 @@
+package io.mosip.esignet.controllers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.security.web.csrf.DefaultCsrfToken;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.mosip.esignet.services.CacheUtilService;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(value = CsrfController.class)
+public class CsrfControllerTest {
+
+	@Autowired
+	MockMvc mockMvc;	
+
+	@MockBean
+	CacheUtilService cacheUtilService;
+
+	ObjectMapper objectMapper = new ObjectMapper();
+
+	@Test
+	public void getCsrfToken_withValidToken_returnsuccessResponse() throws JsonProcessingException, Exception {
+		CsrfToken csrfToken = new DefaultCsrfToken("headerName", "parameterName", "token");
+		mockMvc.perform(get("/csrf/token").content(objectMapper.writeValueAsString(csrfToken))
+				.contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
+	}
+
+}
