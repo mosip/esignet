4dd0bf7eff261ad826bde42fa66c3c51ccf180f7
syed salman <72004356+syedsalman3753@users.noreply.github.com>
2023-04-12 23:59:51 +0530

    [ MOSIP-26666 ] added onboarder for esignet and resident oidc (#184)
    Co-authored-by: syed-salman-technoforte <syed.salman@technoforte.co.in>
    Signed-off-by: ase-101 <sunkadaeanusha@gmail.com>

---
+++ partner-onboarder/install.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+# Onboards default partners 
+## Usage: ./install.sh [kubeconfig]
+
+if [ $# -ge 1 ] ; then
+  export KUBECONFIG=$1
+fi
+
+echo "Do you have public domain & valid SSL? (Y/n) "
+echo "Y: if you have public domain & valid ssl certificate"
+echo "n: if you don't have public domain & valid ssl certificate"
+read -p "" flag
+
+if [ -z "$flag" ]; then
+  echo "'flag' was provided; EXITING;"
+  exit 1;
+fi
+ENABLE_INSECURE=''
+if [ "$flag" = "n" ]; then
+  ENABLE_INSECURE='--set onboarding.enableInsecure=true';
+fi
+
+NS=esignet
+CHART_VERSION=12.0.1-B3
+
+echo Create $NS namespace
+kubectl create ns $NS
+
+function installing_onboarder() {
+
+  read -p "Is values.yaml for onboarder chart set correctly as part of Pre-requisites?(Y/n) " yn;
+  if [ $yn = "Y" ]; then
+    echo Istio label
+    kubectl label ns $NS istio-injection=disabled --overwrite
+    helm repo update
+
+    echo Copy configmaps
+    kubectl -n $NS --ignore-not-found=true delete cm s3
+    sed -i 's/\r$//' copy_cm.sh
+    ./copy_cm.sh
+
+    echo Copy secrets
+    sed -i 's/\r$//' copy_secrets.sh
+    ./copy_secrets.sh
+
+    echo Onboarding default partners
+    helm -n $NS install esignet-resident-oidc-partner-onboarder mosip/partner-onboarder \
+    --set onboarding.configmaps.s3.s3-host='http://minio.minio:9000' \
+    --set onboarding.configmaps.s3.s3-user-key='admin' \
+    --set onboarding.configmaps.s3.s3-region='' \
+    $ENABLE_INSECURE \
+    -f values.yaml \
+    --version $CHART_VERSION
+
+    echo Reports are moved to S3 under onboarder bucket
+    return 0
+  fi
+}
+
+# set commands for error handling.
+set -e
+set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
+set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable
+set -o errtrace  # trace ERR through 'time command' and other functions
+set -o pipefail  # trace ERR through pipes
+installing_onboarder   # calling function
