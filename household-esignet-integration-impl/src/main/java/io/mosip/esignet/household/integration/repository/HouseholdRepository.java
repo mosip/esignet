fcbe7df924d8d1d4a44ac64978a0c941d889ae8d
Venkata Saidurga Polamraju <saidurgacsea@gmail.com>
2023-05-03 12:30:29 +0530

    MOSIP-26992

---
+++ household-esignet-integration-impl/src/main/java/io/mosip/esignet/household/integration/repository/HouseholdRepository.java
@@ -0,0 +1,9 @@
+package io.mosip.esignet.household.integration.repository;
+
+import io.mosip.esignet.household.integration.entity.HouseholdView;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HouseholdRepository extends JpaRepository<HouseholdView,String> {
+     // HouseholdView findByPhoneNumber(String phoneNumber);
+    HouseholdView findByIdNumberAndPassword(String idNumber, String password);
+}
---
+++ household-esignet-integration-impl/src/test/java/io/mosip/esignet/household/integration/service/HouseholdAuthenticatorTest.java
@@ -0,0 +1,4 @@
+package io.mosip.esignet.household.integration.service;
+
+public class HouseholdAuthenticatorTest {
+}
