2cc4fa5722d209a95bb6121c43c3366b87e2f033
pvsaidurga <132046494+pvsaidurga@users.noreply.github.com>
2023-05-15 07:14:11 +0530

    [ES-49] MOSIP-26992 (#207)
    * [ES-49]
    
    * [ES-49]
    
    ---------
    
    Signed-off-by: Anusha Sunkada <sunkadaeanusha@gmail.com>
    Co-authored-by: Anusha Sunkada <sunkadaeanusha@gmail.com>

--- household-esignet-integration-impl/src/test/java/io/mosip/esignet/household/integration/TestApplication.java
+++ household-esignet-integration-impl/src/test/java/io/mosip/esignet/household/integration/TestApplication.java
@@ -1,8 +1,13 @@
 package io.mosip.esignet.household.integration;
 
+import io.mosip.esignet.api.dto.AuditDTO;
+import io.mosip.esignet.api.spi.AuditPlugin;
+import io.mosip.esignet.api.util.Action;
+import io.mosip.esignet.api.util.ActionStatus;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.ActiveProfiles;
 
 @Slf4j
@@ -11,5 +16,20 @@ import org.springframework.test.context.ActiveProfiles;
 public class TestApplication {
     public static void main(String[] args) {
         SpringApplication.run(TestApplication.class, args);
+    }
+
+    @Bean
+    public AuditPlugin loggerAuditWrapper() {
+        return new AuditPlugin() {
+            @Override
+            public void logAudit(Action action, ActionStatus status, AuditDTO audit, Throwable t) {
+                //do nothing
+            }
+
+            @Override
+            public void logAudit(String username, Action action, ActionStatus status, AuditDTO audit, Throwable t) {
+                //do nothing
+            }
+        };
     }
 }
