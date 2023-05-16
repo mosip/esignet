a4a43ee3bc353eb73a7fde2c6480bb7c0cb880aa
kaifk468 <74772315+kaifk468@users.noreply.github.com>
2023-05-16 15:39:21 +0530

    Mosip 26992 local4 (#212)
    * [ES-51] api testing is done
    
    * [ES-51] remove changes after api testing
    
    * [ES-51]

--- household-esignet-integration-impl/src/test/java/io/mosip/esignet/household/integration/HouseholdViewRepositoryTest.java
+++ household-esignet-integration-impl/src/test/java/io/mosip/esignet/household/integration/HouseholdViewRepositoryTest.java
@@ -3,32 +3,18 @@ package io.mosip.esignet.household.integration;
 import io.mosip.esignet.household.integration.entity.HouseholdView;
 import io.mosip.esignet.household.integration.repository.HouseholdViewRepository;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
 import java.util.Optional;
+
 @RunWith(SpringRunner.class)
 @DataJpaTest
 public class HouseholdViewRepositoryTest {
     @Autowired
     private HouseholdViewRepository householdViewRepository;
-
-    @Before
-    public void setUp() {
-        HouseholdView householdView = new HouseholdView();
-        householdView.setHouseholdId(1111112L);
-        householdView.setGroupName("abc");
-        householdView.setPhoneNumber("123456890");
-        householdView.setIdNumber("H01");
-        householdView.setPassword("Household1");
-        householdView.setTamwiniConsented(true);
-        householdView = householdViewRepository.saveAndFlush(householdView);
-    }
-
 
     @Test
     public void householdView_withValidDetail_thenPass() {
@@ -39,11 +25,8 @@ public class HouseholdViewRepositoryTest {
         result = householdViewRepository.findById(11L);
         Assert.assertFalse(result.isPresent());
 
-        result = householdViewRepository.findByIdNumberAndPassword("H01", "Household1");
+        result = householdViewRepository.findByIdNumber("H01");
         Assert.assertTrue(result.isPresent());
-
-        result = householdViewRepository.findByIdNumberAndPassword("H01", "Household2");
-        Assert.assertFalse(result.isPresent());
 
     }
 }
--- household-esignet-integration-impl/src/test/resources/schema.sql
+++ household-esignet-integration-impl/src/test/resources/schema.sql
@@ -1,9 +1,34 @@
-CREATE TABLE IF NOT EXISTS household_view (
-household_ID BIGINT,
-group_name character varying(256) NOT NULL,
-phone_number character varying(256) NOT NULL,
-ID_number character varying(256) NOT NULL,
-password character varying(256) NOT NULL,
-tamwini_consented boolean default true,
-CONSTRAINT pk_household PRIMARY KEY (household_ID)
+--CREATE VIEW household_view (
+--household_ID BIGINT,
+--group_name character varying(256) NOT NULL,
+--phone_number character varying(256) NOT NULL,
+--ID_number character varying(256) NOT NULL,
+--password character varying(256) NOT NULL,
+--tamwini_consented boolean default true,
+--CONSTRAINT pk_household PRIMARY KEY (household_ID)
+--);
+CREATE TABLE household (
+    household_ID BIGINT PRIMARY KEY,
+    group_name character varying(256) NOT NULL,
+    phone_number character varying(256) NOT NULL,
+    ID_number character varying(256) NOT NULL,
+    password character varying(256) NOT NULL,
+    tamwini_consented boolean default true
 );
+
+INSERT INTO household(household_ID,group_name,phone_number,ID_number,password) VALUES
+(1111112L,'test','1234567890','H01','12345');
+
+--INSERT INTO household(household_ID,group_name,phone_number,ID_number,password) VALUES
+--(12,"TEST2","12322","12345","123415234");
+
+CREATE VIEW  household_view AS
+SELECT
+    household_ID,
+    group_name,
+    phone_number,
+    ID_number,
+    password,
+    tamwini_consented
+FROM
+    household;
