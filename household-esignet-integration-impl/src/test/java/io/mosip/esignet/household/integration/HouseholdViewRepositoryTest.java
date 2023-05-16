9230011d60c95570bbe99b35d95bb75fe58299fc
Mohd Kaif Siddique <kaifk468@gmail.com>
2023-05-16 15:16:16 +0530

    [ES-51] remove changes after api testing

--- household-esignet-integration-impl/src/test/java/io/mosip/esignet/household/integration/HouseholdViewRepositoryTest.java
+++ household-esignet-integration-impl/src/test/java/io/mosip/esignet/household/integration/HouseholdViewRepositoryTest.java
@@ -3,32 +3,18 @@ package io.mosip.esignet.household.integration;
 import io.mosip.esignet.household.integration.entity.HouseholdView;
 import io.mosip.esignet.household.integration.repository.HouseholdViewRepository;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
 import java.util.Optional;
+
 @RunWith(SpringRunner.class)
 @DataJpaTest
 public class HouseholdViewRepositoryTest {
     @Autowired
     private HouseholdViewRepository householdViewRepository;
-
-//    @Before
-//    public void setUp() {
-//        HouseholdView householdView = new HouseholdView();
-//        householdView.setHouseholdId(1111112L);
-//        householdView.setGroupName("abc");
-//        householdView.setPhoneNumber("123456890");
-//        householdView.setIdNumber("H01");
-//        householdView.setPassword("Household1");
-//        householdView.setTamwiniConsented(true);
-//        householdView = householdViewRepository.saveAndFlush(householdView);
-//    }
-
 
     @Test
     public void householdView_withValidDetail_thenPass() {
