64c625df3116b714b8cf8caffd5870d3359266ba
Loganathan Sekar <42532387+LoganathanSekar7627@users.noreply.github.com>
2023-05-17 06:29:52 +0530

    Mosip 27084 1.0.0 auth mosip 26992 branch (#213)
    * Fix for send otp for new auth wrapper
    
    * MOSIP-27084 added id-auth wrapper for 1.1.5.5 IDA and esignet
    
    * Refactored module name
    
    * Refactoring the auth wrapper
    
    * Revert "Fix for send otp for new auth wrapper"
    
    This reverts commit 126a0ad4d082b5a4cc52af9ffd7b2b85dbc8760b.
    
    * Commenting auth common and auth core dependencies to avoid dependency loading issue
    
    * Delete .classpath
    
    * Delete .project
    
    * Added version
    
    * Uncommenting code for kyc exchange enabling
    
    * Revert "Uncommenting code for kyc exchange enabling"
    
    This reverts commit d75db54cfa179caaa9f946203dce09894a50d836.
    
    * Ignored tests
    
    ---------
    
    Co-authored-by: Loganathan Sekar <m1047627@mindtree.com>
    Co-authored-by: Anusha Sunkada <sunkadaeanusha@gmail.com>

---
+++ esignet-ida115-integration-impl/src/main/java/io/mosip/authentication/esignet/integration/config/IdaConfig.java
@@ -0,0 +1,144 @@
+package io.mosip.authentication.esignet.integration.config;
+
+import org.mockito.Mockito;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+
+//import io.mosip.authentication.common.service.factory.RestRequestFactory;
+//import io.mosip.authentication.common.service.helper.IdInfoHelper;
+//import io.mosip.authentication.common.service.helper.RestHelper;
+//import io.mosip.authentication.common.service.integration.MasterDataManager;
+//import io.mosip.authentication.common.service.integration.OTPManager;
+//import io.mosip.authentication.common.service.repository.OtpTxnRepository;
+//import io.mosip.authentication.common.service.repository.UinHashSaltRepo;
+//import io.mosip.authentication.common.service.transaction.manager.IdAuthSecurityManager;
+import io.mosip.authentication.esignet.integration.service.TokenIdManager;
+import io.mosip.kernel.cbeffutil.impl.CbeffImpl;
+import io.mosip.kernel.core.cbeffutil.spi.CbeffUtil;
+import io.mosip.kernel.cryptomanager.service.CryptomanagerService;
+import io.mosip.kernel.cryptomanager.service.impl.CryptomanagerServiceImpl;
+import io.mosip.kernel.tokenidgenerator.generator.TokenIDGenerator;
+import io.mosip.kernel.tokenidgenerator.service.TokenIDGeneratorService;
+import io.mosip.kernel.tokenidgenerator.service.impl.TokenIDGeneratorServiceImpl;
+import io.mosip.kernel.zkcryptoservice.service.spi.ZKCryptoManagerService;
+
+@EnableCaching
+@Configuration
+//TODO commented assuming auth only used
+//@Import(value= {IDAMappingConfig.class})
+public class IdaConfig {
+
+	@Bean
+	public TokenIDGeneratorService getTokenIdGeneratorService() {
+		return new TokenIDGeneratorServiceImpl();
+	}
+	
+	@Bean
+	public TokenIdManager getTokenIdManager() {
+		return new TokenIdManager();
+	}
+
+	@Bean
+	public TokenIDGenerator getTokenIdGenerator() {
+		return new TokenIDGenerator();
+	}
+
+	@Bean
+	public CryptomanagerService getCryptomanagerService() {
+		return new CryptomanagerServiceImpl();
+	}
+	
+//	@Lazy
+//	@Bean
+//	public IdInfoHelper getIdInfoHelper() {
+//		return new IdInfoHelper();
+//	}
+
+//	@Lazy
+//	@Bean
+//	public IdInfoFetcher getIdInfoFetcher() {
+//		return new IdInfoFetcherImpl();
+//	}
+
+//	@Lazy
+//	@Bean
+//	public OTPManager getOTPManager() {
+//		return new OTPManager();
+//	}
+
+//	@Lazy
+//	@Bean
+//	public CbeffUtil getCbeffUtil() {
+//		return new CbeffImpl();
+//	}
+//
+//	@Lazy
+//	@Bean
+//	public MasterDataManager getMasterDataManager() {
+//		return new MasterDataManager();
+//	}
+
+//	@Lazy
+//	@Bean
+//	public MasterDataCache getMasterDataCache() {
+//		return new MasterDataCache() {
+//			@Override
+//			public void loadMasterData() throws IdAuthenticationBusinessException {
+//				//Do nothing
+//			}
+//		};
+//	}
+
+//	@Lazy
+//	@Bean
+//	public RestRequestFactory getRestRequestFactory() {
+//		return new RestRequestFactory();
+//	}
+//
+//	@Lazy
+//	@Bean
+//	public IdAuthSecurityManager getIdAuthSecurityManager() {
+//		return new IdAuthSecurityManager();
+//	}
+	
+
+
+//	@Bean("external")
+//	public RestHelper getRestHelper() {
+//		// Just using mock rest helper as it is not used here
+//		return Mockito.mock(RestHelper.class);
+//	}
+
+//	@Bean
+//	public NotificationService getNotificationService() {
+//		// Just using mock rest helper as it is not used here
+//		return Mockito.mock(NotificationService.class);
+//	}
+
+//	@Bean
+//	public UinHashSaltRepo getUinHashSaltRepo() {
+//		// Just using mock rest helper as it is not used here
+//		return Mockito.mock(UinHashSaltRepo.class);
+//	}
+//
+//	@Bean
+//	public DemoNormalizer getDemoNormalizer() {
+//		// Just using mock rest helper as it is not used here
+//		return Mockito.mock(DemoNormalizer.class);
+//	}
+
+//	@Bean
+//	public ZKCryptoManagerService getZKCryptoManagerService() {
+//		// Just using mock rest helper as it is not used here
+//		return Mockito.mock(ZKCryptoManagerService.class);
+//	}
+//
+//	@Bean
+//	public OtpTxnRepository getOtpTxnRepository() {
+//		// Just using mock rest helper as it is not used here
+//		return Mockito.mock(OtpTxnRepository.class);
+//	}
+
+}
