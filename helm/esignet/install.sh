62fede3e7e87580ade2471fdefc66ef4e2f187b5
syed-salman-technoforte <syed.salman@technoforte.co.in>
2023-05-30 15:42:28 +0530

    [MOSIP-27593] Updated install.sh & README.md

--- helm/esignet/install.sh
+++ helm/esignet/install.sh
@@ -14,8 +14,22 @@ CHART_VERSION=1.0.1
 echo Copy configmaps
 ./copy_cm.sh
 
+echo "Do you have public domain & valid SSL? (Y/n) "
+echo "Y: if you have public domain & valid ssl certificate"
+echo "n: If you don't have a public domain and a valid SSL certificate. Note: It is recommended to use this option only in development environments."
+read -p "" flag
+
+if [ -z "$flag" ]; then
+  echo "'flag' was provided; EXITING;"
+  exit 1;
+fi
+ENABLE_INSECURE=''
+if [ "$flag" = "n" ]; then
+  ENABLE_INSECURE='--set enable_insecure=true';
+fi
+
 echo Installing esignet
-helm -n $NS install esignet mosip/esignet --version $CHART_VERSION
+helm -n $NS install esignet mosip/esignet --version $CHART_VERSION $ENABLE_INSECURE
 
 kubectl -n $NS  get deploy -o name |  xargs -n1 -t  kubectl -n $NS rollout status
 
