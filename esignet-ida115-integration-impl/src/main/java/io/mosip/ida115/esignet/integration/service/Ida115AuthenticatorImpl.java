5df0f61dfb6364e6dbb9a6aacb5333d623e2407e
Himaja Dhanyamraju <43470317+HimajaDhanyamraju2@users.noreply.github.com>
2023-05-31 15:02:44 +0530

    Changes in Ida115AuthenticatorImpl (#225)
    Co-authored-by: M1044292 <M1044292@C1ML54597.mindtree.com>

--- esignet-ida115-integration-impl/src/main/java/io/mosip/ida115/esignet/integration/service/Ida115AuthenticatorImpl.java
+++ esignet-ida115-integration-impl/src/main/java/io/mosip/ida115/esignet/integration/service/Ida115AuthenticatorImpl.java
@@ -516,11 +516,7 @@ public class Ida115AuthenticatorImpl implements Authenticator {
 				}
 			}
 		} else {
-			if (consentedAttribute.equals(consentedAddressAttributeName)) {
-				handleAttributeWithSubAttributes(mappedConsentedLocales, idSchemaAttributes, idInfoNameAndLangCorrected, respMap, consentedAddressAttributeName, addressValueSeparator, addressSubsetAttributes);
-			}/* else if (consentedAttribute.equals(consentedNameAttributeName)) {
-				handleAttributeWithSubAttributes(mappedConsentedLocales, idSchemaAttributes, idInfoNameAndLangCorrected, respMap, consentedNameAttributeName, addressNameSeparator, nameSubsetAttributes);
-			}*/
+			handleAttributeWithSubAttributes(mappedConsentedLocales, idSchemaAttributes, idInfoNameAndLangCorrected, respMap, consentedAddressAttributeName, addressValueSeparator, addressSubsetAttributes);
 		}
 	}
 	
@@ -591,9 +587,9 @@ public class Ida115AuthenticatorImpl implements Authenticator {
 		//String trimmedValue = identityInfoValueStr.substring(0, identityInfoValueStr.lastIndexOf(addressValueSeparator));
 		attributesMap.put(FORMATTED + localeAppendValue, identityInfoValue.toString());
 		if (langCodeFound && addLocale) {
-			respMap.put(consentedAttributeName + localeAppendValue, attributesMap);
+			respMap.put(consentedAttributeName + localeAppendValue, (consentedAttributeName.equals(consentedAddressAttributeName)) ? attributesMap : identityInfoValue.toString());
 		} else {
-			respMap.put(consentedAttributeName, attributesMap);
+			respMap.put(consentedAttributeName, (consentedAttributeName.equals(consentedAddressAttributeName)) ? attributesMap : identityInfoValue.toString());
 		}
 	}
 	
