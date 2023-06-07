3f285879e3c647825be53d9c52a14ae9b63ff7d8
Akila Lakshmanan <77330852+akilalakshmanan@users.noreply.github.com>
2023-06-07 09:15:08 +0530

    [MOSIP-25835] Updated Readme file for adding misp partner license key (#222)
    * [MOSIP-25835] Updated Readme file for adding misp partner license key module
    
    * [MOSIP-25835] Updated Readme file
    
    * [MOSIP-25835] Updated Readme file
    
    * [MOSIP-25835] Moved esignet misp license key automation script to esignet partner-onboarder
    
    * [MOSIP-25835] Removed the check condition to add misp key
    
    * [MOSIP-25835] Updated print statements
    
    ---------
    
    Co-authored-by: akilalakshmanan <Akila.L@iiitb.ac.in>
    Signed-off-by: ase-101 <sunkadaeanusha@gmail.com>

--- helm/esignet/install.sh
+++ helm/esignet/install.sh
@@ -32,18 +32,6 @@ if [ "$flag" = "n" ]; then
   ENABLE_INSECURE='--set enable_insecure=true';
 fi
 
-MISPKEY=$(bash misp_key.sh)
-echo "MISP License key is: $MISPKEY"
-
-echo Setting up onboarder-keys secrets
-kubectl -n $NS create secret generic onboarder-keys --from-literal=mosip-esignet-misp-key=$MISPKEY --dry-run=client -o yaml | kubectl apply -f -
-
-./copy_cm_func.sh secret onboarder-keys esignet config-server
-
-kubectl -n config-server set env --keys=mosip-esignet-misp-key --from secret/onboarder-keys deployment/config-server --prefix=SPRING_CLOUD_CONFIG_SERVER_OVERRIDES_
-
-kubectl -n config-server get deploy -o name |  xargs -n1 -t  kubectl -n config-server rollout status
-
 echo Installing esignet
 helm -n $NS install esignet mosip/esignet --version $CHART_VERSION $ENABLE_INSECURE
 
