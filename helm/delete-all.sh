1a94377957a4ee6096b2de79c14ef99287035cd5
Rakshithb1 <79500257+Rakshithb1@users.noreply.github.com>
2023-07-11 13:18:11 +0530

    [MOSIP-27549] added error handling (#258)
    * [MOSIP-27549] added error handling
    
    * [MOSIP-27549] updated copy_cm.sh

--- helm/delete-all.sh
+++ helm/delete-all.sh
@@ -7,19 +7,30 @@ if [ $# -ge 1 ] ; then
   export KUBECONFIG=$1
 fi
 
-ROOT_DIR=`pwd`
+function Deleting_All() {
+  ROOT_DIR=`pwd`
 
-declare -a module=("redis"
-                   "esignet"
-		   "oidc-ui"
-                   )
+  declare -a module=("redis"
+                     "esignet"
+         "oidc-ui"
+                     )
 
-echo Installing esignet services
+  echo Installing esignet services
 
-for i in "${module[@]}"
-do
-  cd $ROOT_DIR/"$i"
-  ./delete.sh
-done
+  for i in "${module[@]}"
+  do
+    cd $ROOT_DIR/"$i"
+    ./delete.sh
+  done
 
-echo All esignet services deleted sucessfully.
+  echo All esignet services deleted sucessfully.
+  return 0
+}
+
+# set commands for error handling.
+set -e
+set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
+set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable
+set -o errtrace  # trace ERR through 'time command' and other functions
+set -o pipefail  # trace ERR through pipes
+Deleting_All   # calling function
--- helm/redis/install.sh
+++ helm/redis/install.sh
@@ -12,18 +12,29 @@ CHART_VERSION=17.3.14
 echo Create $NS namespace
 kubectl create ns $NS
 
-echo Istio label
-kubectl label ns $NS istio-injection=enabled --overwrite
+function installing_redis() {
+  echo Istio label
+  kubectl label ns $NS istio-injection=enabled --overwrite
 
-echo Updating helm repos
-helm repo add bitnami https://charts.bitnami.com/bitnami
-helm repo update
+  echo Updating helm repos
+  helm repo add bitnami https://charts.bitnami.com/bitnami
+  helm repo update
 
-echo Installing redis
-helm -n $NS install redis bitnami/redis --wait --version $CHART_VERSION
+  echo Installing redis
+  helm -n $NS install redis bitnami/redis --wait --version $CHART_VERSION
 
-../copy_cm_func.sh secret redis redis config-server
+  ../copy_cm_func.sh secret redis redis config-server
 
-kubectl -n config-server set env --keys=redis-password --from secret/redis deployment/config-server --prefix=SPRING_CLOUD_CONFIG_SERVER_OVERRIDES_
+  kubectl -n config-server set env --keys=redis-password --from secret/redis deployment/config-server --prefix=SPRING_CLOUD_CONFIG_SERVER_OVERRIDES_
 
-echo Installed redis service
+  echo Installed redis service
+  return 0
+}
+
+# set commands for error handling.
+set -e
+set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
+set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable
+set -o errtrace  # trace ERR through 'time command' and other functions
+set -o pipefail  # trace ERR through pipes
+installing_redis   # calling function
