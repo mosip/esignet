fac692328f8794abbaab3e3eb42c954a6c16f84a
Mohd Kaif Siddique <kaifk468@gmail.com>
2023-08-07 12:45:48 +0530

    review changes

--- db_scripts/mosip_esignet/ddl/rollback_add_thumbprints_public_key_registry.sql
+++ db_scripts/mosip_esignet/ddl/rollback_add_thumbprints_public_key_registry.sql
@@ -1,6 +1,7 @@
 -- This script removes the 'thumbprint' column from the 'public_key_registry' table
 -- and drops the 'generate_thumbprint' function, effectively undoing the changes made by the previous script.
 
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA esignet TO sysadmin;
 --  Drop the thumbprint column from the public_key_registry table
 ALTER TABLE public_key_registry
 DROP COLUMN thumbprint;
