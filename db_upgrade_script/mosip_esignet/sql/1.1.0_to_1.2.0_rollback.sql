02298650b2b6326a7f781d449447737629dbaf56
Mohd Kaif Siddique <kaifk468@gmail.com>
2023-08-14 12:23:57 +0530

    reveiw changes

--- db_upgrade_script/mosip_esignet/sql/1.1.0_to_1.2.0_rollback.sql
+++ db_upgrade_script/mosip_esignet/sql/1.1.0_to_1.2.0_rollback.sql
@@ -3,3 +3,7 @@
 --  Drop the thumbprint column from the public_key_registry table
 ALTER TABLE public_key_registry
 DROP COLUMN thumbprint;
+--  Drop the generate_thumbprint function
+DROP FUNCTION IF EXISTS generate_thumbprint(text);
+---Drop the Extension pgcrypto
+DROP EXTENSION IF EXISTS pgcrypto;
