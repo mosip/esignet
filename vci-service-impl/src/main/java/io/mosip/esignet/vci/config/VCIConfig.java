8c5f750c28a9ba5d2345b1423cefc8fc135221b1
ase-101 <>
2023-08-31 18:05:50 +0530

    ES-4

--- vci-service-impl/src/main/java/io/mosip/esignet/vci/config/VCIConfig.java
+++ vci-service-impl/src/main/java/io/mosip/esignet/vci/config/VCIConfig.java
@@ -6,11 +6,7 @@
 package io.mosip.esignet.vci.config;
 
 import io.mosip.esignet.core.dto.vci.ParsedAccessToken;
-import io.mosip.esignet.vci.filter.AccessTokenValidationFilter;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
