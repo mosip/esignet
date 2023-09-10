94739389145a1a21e93b514cef61b7c46f86ce17
Anusha Sunkada <sunkadaeanusha@gmail.com>
2023-09-10 11:51:10 +0530

    Test cases added (#350)
    Co-authored-by: ase-101 <>

--- esignet-integration-api/src/main/java/io/mosip/esignet/api/spi/VCIssuancePlugin.java
+++ esignet-integration-api/src/main/java/io/mosip/esignet/api/spi/VCIssuancePlugin.java
@@ -1,3 +1,8 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
 package io.mosip.esignet.api.spi;
 
 import foundation.identity.jsonld.JsonLDObject;
@@ -6,9 +11,6 @@ import io.mosip.esignet.api.dto.VCResult;
 
 import java.util.Map;
 
-/**
- *
- */
 public interface VCIssuancePlugin {
 
     /**
