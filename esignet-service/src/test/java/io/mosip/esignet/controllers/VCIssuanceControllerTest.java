2ba29e8b6527d98b6aff3677caad52df86bca3c6
ase-101 <>
2023-09-10 13:33:27 +0530

    Test case fix

--- esignet-service/src/test/java/io/mosip/esignet/controllers/VCIssuanceControllerTest.java
+++ esignet-service/src/test/java/io/mosip/esignet/controllers/VCIssuanceControllerTest.java
@@ -17,12 +17,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.Map;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
