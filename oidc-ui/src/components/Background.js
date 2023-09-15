74b319805fe1fba3113f67b52716479dc46e8196
Zeeshan Mehboob <zeeshan.mehboob@infosys.com>
2023-09-15 18:36:42 +0530

    [MODIFIED] rtl changes for esignet images
    Signed-off-by: ase-101 <sunkadaeanusha@gmail.com>

--- oidc-ui/src/components/Background.js
+++ oidc-ui/src/components/Background.js
@@ -14,17 +14,17 @@ export default function Background({
   return (
     <>
       <section className="text-gray-600 mt-4 body-font">
-        <div className="container flex mx-auto px-5 sm:flex-row flex-col rtl:sm:flex-row-reverse">
-          <div className="flex justify-center lg:mt-32 mb:mt-0 lg:w-1/2 md:w-1/2 md:block sm:w-1/2 sm:block hidden w-5/6 mt-20 mb-10 md:mb-0">
+        <div className="container flex mx-auto px-5 sm:flex-row flex-col">
+          <div className="flex justify-center lg:mt-20 mb:mt-0 lg:w-1/2 md:w-1/2 md:block sm:w-1/2 sm:block hidden w-5/6 mt-20 mb-10 md:mb-0">
             <div>
               <img
-                className="object-contain"
+                className="object-contain rtl:scale-x-[-1]"
                 alt={t("backgroud_image_alt")}
                 src={backgroundImgPath}
               />
             </div>
           </div>
-          <div className="rounded overflow-hidden shadow-lg py-4 w-full md:w-3/6 sm:w-1/2 sm:max-w-sm lg:ml-auto md:ml-auto">
+          <div className="rounded overflow-hidden shadow-lg py-4 w-full md:w-3/6 sm:w-1/2 sm:max-w-sm ltr:md:ml-auto rtl:md:mr-auto">
             <div className="flex flex-col flex-grow lg:px-5 md:px-4 sm:px-3 px-3">
               <div className="w-full">
                 <h1 className="flex text-center justify-center title-font sm:text-base text-base mb-3 font-medium text-gray-900">
