0dd297dddfe1b4c6ad13d2b2acc276b38f97f175
Zeeshan Mehboob <zeeshan.mehboob@infosys.com>
2023-09-21 14:01:29 +0530

    [ADDED] configuration for brand logo, background, login card list
    Signed-off-by: Zeeshan Mehboob <zmahboob00@gmail.com>

--- oidc-ui/src/components/Background.js
+++ oidc-ui/src/components/Background.js
@@ -11,18 +11,14 @@ export default function Background({
   i18nKeyPrefix = "header",
 }) {
   const { t } = useTranslation("translation", { keyPrefix: i18nKeyPrefix });
-  const headerHeight = document.getElementById("navbar-header")?.offsetHeight;
-  const sectionStyle = {
-    height: "calc(100% - " + headerHeight + "px)",
-  };
+
+  const backgroundColor = process.env.REACT_APP_LOGIN_BACKGROUND ?? "#FFFFFF";
+  {/* height is used by subtracting navbar height  */}
+  const sectionClass = `text-gray-600 pt-4 body-font bg-[${backgroundColor}] h-[calc(100%-54px)]`;
 
   return (
     <>
-      {/* height is used by subtracting navbar height  */}
-      <section
-        className="text-gray-600 pt-4 body-font"
-        style={sectionStyle}
-      >
+      <section className={sectionClass}>
         <div className="container flex mx-auto px-5 sm:flex-row flex-col">
           <div className="flex justify-center m-10 lg:mt-20 mb:mt-0 lg:w-1/2 md:w-1/2 md:block sm:w-1/2 sm:block hidden w-5/6 mt-20 mb-10 md:mb-0">
             <div>
@@ -33,7 +29,7 @@ export default function Background({
               />
             </div>
           </div>
-          <div className="rounded shadow-lg py-4 w-full md:w-3/6 sm:w-1/2 sm:max-w-sm bg-white">
+          <div className="rounded overflow-hidden shadow-lg py-4 w-full md:w-3/6 sm:w-1/2 sm:max-w-sm bg-white">
             <div className="flex flex-col flex-grow lg:px-5 md:px-4 sm:px-3 px-3">
               <div className="w-full">
                 <h1 className="flex text-center justify-center title-font sm:text-base text-base mb-3 font-medium text-gray-900">
@@ -41,9 +37,17 @@ export default function Background({
                 </h1>
               </div>
               <div className="w-full flex mb-4 justify-center items-center">
-                <img className="h-20 w-32 object-contain" src={clientLogoPath} alt={clientName} />
+                <img
+                  className="h-20 w-32 object-contain"
+                  src={clientLogoPath}
+                  alt={clientName}
+                />
                 <span className="text-5xl flex mx-5">&#8651;</span>
-                <img className="h-20 w-32 object-contain" src={logoPath} alt={t("logo_alt")} />
+                <img
+                  className="h-20 w-32 object-contain"
+                  src={logoPath}
+                  alt={t("logo_alt")}
+                />
               </div>
               <div
                 className="h-5 text-black -mx-5 mb-2"
