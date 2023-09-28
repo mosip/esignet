9258229327810f7493622f653a2b87a4ee0e2c74
Mohd Kaif Siddique <kaifk468@gmail.com>
2023-09-28 19:22:17 +0530

    added codeChallenge validation

--- esignet-core/src/main/java/io/mosip/esignet/core/validator/CodeChallengeValidator.java
+++ esignet-core/src/main/java/io/mosip/esignet/core/validator/CodeChallengeValidator.java
@@ -2,11 +2,11 @@ package io.mosip.esignet.core.validator;
 
 import io.mosip.esignet.core.dto.OAuthDetailRequestV2;
 import org.springframework.util.StringUtils;
-
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 public class CodeChallengeValidator implements ConstraintValidator<CodeChallenge, OAuthDetailRequestV2> {
+
     @Override
     public boolean isValid(OAuthDetailRequestV2 value, ConstraintValidatorContext context) {
         String codeChallenge = value.getCodeChallenge();
