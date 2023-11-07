a0abdb205f9a800a57cd7e462e5869e70b84bda5
Zeeshan Mehboob <zmahboob00@gmail.com>
2023-11-07 14:52:57 +0530

    [ADDED] configurable footer and side logo
    Signed-off-by: Zeeshan Mehboob <zmahboob00@gmail.com>

--- oidc-ui/src/components/Background.js
+++ oidc-ui/src/components/Background.js
@@ -10,19 +10,20 @@ export default function Background({
 }) {
   const { t } = useTranslation("translation", { keyPrefix: i18nKeyPrefix });
 
+  const backgroundLogo = process.env.REACT_APP_BACKGROUND_LOGO === "true";
+  const sectionClass = process.env.REACT_APP_FOOTER === "true" ? "flexible-header-footer" : "flexible-header-only";
+  
   return (
     <>
     {/* height is used by subtracting navbar height  */}
-      <section className="text-gray-600 pt-4 body-font h-[calc(100%-55px)] section-background">
-        <div className="container flex mx-auto px-5 sm:flex-row flex-col">
-          <div className="flex justify-center m-10 lg:mt-20 mb:mt-0 lg:w-1/2 md:w-1/2 md:block sm:w-1/2 sm:block hidden w-5/6 mt-20 mb-10 md:mb-0">
-            <div>
-              <img
-                className="background-logo object-contain rtl:scale-x-[-1]"
-                alt={t("backgroud_image_alt")}
-              />
-            </div>
-          </div>
+      <section className={"text-gray-600 pt-4 body-font section-background "+sectionClass}>
+        <div className="container justify-center flex mx-auto px-5 sm:flex-row flex-col">
+          {backgroundLogo && (<div className="flex justify-center m-10 lg:mt-20 mb:mt-0 lg:w-1/2 md:w-1/2 md:block sm:w-1/2 sm:block hidden w-5/6 mt-20 mb-10 md:mb-0">
+            <img
+              className="background-logo object-contain rtl:scale-x-[-1]"
+              alt={t("backgroud_image_alt")}
+            />
+          </div>)}
           <div className="rounded shadow-lg py-4 w-full md:w-3/6 sm:w-1/2 sm:max-w-sm bg-white">
             <div className="flex flex-col flex-grow lg:px-5 md:px-4 sm:px-3 px-3">
               <div className="w-full">
