daf860b10c157ded6caceeb14a56b64227d5af95
GurukiranP <talk2gurukiran@gmail.com>
2023-11-28 10:41:16 +0530

    Added console logs to check for the footer
    Signed-off-by: GurukiranP <talk2gurukiran@gmail.com>
    Signed-off-by: ase-101 <sunkadaeanusha@gmail.com>

--- oidc-ui/src/services/utilService.js
+++ oidc-ui/src/services/utilService.js
@@ -1,5 +1,6 @@
 const getBooleanValue = (inputValue) => {
     if (typeof(inputValue) === 'undefined' || inputValue === null || inputValue === '') {
+        console.log("no input provided")
         return false;
     }
     return /^true$/i.test(inputValue);
