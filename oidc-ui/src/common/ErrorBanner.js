99f4b8824f9c92c6731c4a0d79864bcd4ae9dddc
Zeeshan Mehboob <zmahboob00@gmail.com>
2023-11-28 15:24:04 +0530

    [ADDED] error banner, password validation, show hide password
    Signed-off-by: Zeeshan Mehboob <zmahboob00@gmail.com>

---
+++ oidc-ui/src/common/ErrorBanner.js
@@ -0,0 +1,30 @@
+import { useTranslation } from "react-i18next";
+
+const ErrorBanner = ({
+  showBanner,
+  errorCode,
+  defaultMsg,
+  onCloseHandle,
+  customClass = "",
+  i18nKeyPrefix = "errors",
+}) => {
+  const { t } = useTranslation("translation", { keyPrefix: i18nKeyPrefix });
+
+  return showBanner && (
+    <div
+      className={
+        "flex justify-between items-center px-5 lg:-mx-5 md:-mx-4 sm:-mx-3 -mx-3 error-banner " +
+        customClass
+      }
+    >
+      <div className="error-banner-text">{t(errorCode, defaultMsg)}</div>
+      <img
+        onClick={onCloseHandle}
+        className="h-2.5 w-2.5"
+        src="images/cross_icon.svg"
+      />
+    </div>
+  );
+};
+
+export default ErrorBanner;
