a25510f70c9788a6fddcd4d16ae1117105aaae88
Zeeshan Mehboob <zmahboob00@gmail.com>
2023-11-30 09:01:22 +0530

    [ADDED] acr based back button
    Signed-off-by: Zeeshan Mehboob <zmahboob00@gmail.com>

--- oidc-ui/src/services/openIDConnectService.js
+++ oidc-ui/src/services/openIDConnectService.js
@@ -1,6 +1,12 @@
 import { Buffer } from "buffer";
-import sha256 from 'crypto-js/sha256';
-import Base64 from 'crypto-js/enc-base64';
+import sha256 from "crypto-js/sha256";
+import Base64 from "crypto-js/enc-base64";
+import {
+  walletConfigKeys,
+  validAuthFactors,
+  configurationKeys,
+  modalityIconPath,
+} from "../constants/clientConstants";
 
 class openIDConnectService {
   constructor(oAuthDetails, nonce, state) {
@@ -77,6 +83,60 @@ class openIDConnectService {
     // Replace '+' with '-' and '/' with '_' to convert to base64 URL encoding
     hashB64 = hashB64.replace(/\+/g, "-").replace(/\//g, "_");
     return hashB64;
+  };
+
+  /**
+   * Convert wallet object to common authfactor format
+   * @param {walletConfigKeys} wla an object with name, logo url, deep-link-uri & download-uri
+   * @returns common object for authfactor detail with label, value, icon & id
+   */
+  wlaToAuthfactor = (wla) => {
+    return {
+      label: wla[walletConfigKeys.walletName],
+      value: { ...wla, type: "WLA" },
+      icon: wla[walletConfigKeys.walletLogoUrl],
+      id: `login_with_${wla[walletConfigKeys.walletName]
+        .replace(" ", "_")
+        .toLowerCase()}`,
+    };
+  };
+
+  /**
+   * Convert an authfactor to a common authfactor format
+   * @param {Object[]} authFactor an array with single object, with type, subtypes & count
+   * @returns common object for authfactor detail with label, value, icon & id
+   */
+  toAuthfactor = (authFactor) => {
+    return {
+      label: authFactor[0].type,
+      value: authFactor[0],
+      icon: modalityIconPath[authFactor[0].type],
+      id: `login_with_${authFactor[0].type.toLowerCase()}`,
+    };
+  };
+
+  /**
+   * Get list of authfactor applicable for the current client
+   * @returns list of authfactor
+   */
+  getAuthFactorList = () => {
+    let loginOptions = [];
+    const wlaList =
+      this.getEsignetConfiguration(configurationKeys.walletConfig) ??
+      process.env.REACT_APP_WALLET_CONFIG;
+    this.oAuthDetails?.authFactors.forEach((authFactor) => {
+      const authFactorType = authFactor[0].type;
+      if (validAuthFactors[authFactorType]) {
+        if (authFactorType === validAuthFactors.WLA) {
+          wlaList.forEach((wla) =>
+            loginOptions.push(this.wlaToAuthfactor(wla))
+          );
+        } else {
+          loginOptions.push(this.toAuthfactor(authFactor));
+        }
+      }
+    });
+    return loginOptions;
   };
 }
 
