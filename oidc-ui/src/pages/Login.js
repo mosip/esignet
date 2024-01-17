981f6d03717a7923586b470a9b48cf89cd7e600f
Aravindhan Alagesan <aravindh2090@gmail.com>
2024-01-17 10:33:51 +0530

    Back button issue resolved
    Signed-off-by: Aravindhan Alagesan <aravindh2090@gmail.com>
    Signed-off-by: Vishwa <visu.vs1@gmail.com>

--- oidc-ui/src/pages/Login.js
+++ oidc-ui/src/pages/Login.js
@@ -183,7 +183,7 @@ export default function LoginPage({ i18nKeyPrefix = "header" }) {
     );
   };
 
-  const handleBackButtonClick = () => {
+  function handleBackButtonClick(){
     setCompToShow(InitiateSignInOptions(handleSignInOptionClick, oidcService));
   };
 
@@ -214,12 +214,9 @@ export default function LoginPage({ i18nKeyPrefix = "header" }) {
         heading={t("login_heading", {
           idProviderName: window._env_.DEFAULT_ID_PROVIDER_NAME,
         })}
-        subheading={t("login_subheading")}
         clientLogoPath={clientLogoURL}
         clientName={clientName}
         component={compToShow}
-        oidcService={oidcService}
-        authService={new authService(null)}
       />
     </>
   );
