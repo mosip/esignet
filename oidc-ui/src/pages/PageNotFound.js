a09ccc99706be46835df4072648fe890cef8d5f0
Zeeshan Mehboob <zmahboob00@gmail.com>
2024-01-17 15:13:01 +0530

    [FIXES] key prefix for page not found
    Signed-off-by: Zeeshan Mehboob <zmahboob00@gmail.com>

--- oidc-ui/src/pages/PageNotFound.js
+++ oidc-ui/src/pages/PageNotFound.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 
-export default function PageNotFoundPage() {
-  const { t } = useTranslation("translation");
+export default function PageNotFoundPage({ i18nKeyPrefix = "errors" }) {
+  const { t } = useTranslation("translation", { keyPrefix: i18nKeyPrefix });
 
   return (
     <div className="multipurpose-login-card w-full m-16 shadow shadow-lg py-24">
@@ -11,8 +11,8 @@ export default function PageNotFoundPage() {
         src="images/under_construction.svg"
         alt="page_not_found"
       />
-      <div className="error-page-header">{t("errors.page_not_exist")}</div>
-      <div className="error-page-detail">{t("errors.navigate_option")}</div>
+      <div className="error-page-header">{t("page_not_exist")}</div>
+      <div className="error-page-detail">{t("navigate_option")}</div>
     </div>
   );
 }
