16868d3d7765f3864f2237cd453998c7f227d59a
pvsaidurga <132046494+pvsaidurga@users.noreply.github.com>
2024-05-01 23:27:06 +0530

    [MOSIP-32633] Added regex validation for KBA (#667)
    * [MOSIP-32633] Added regex validation for KBA
    
    Signed-off-by: Venkata Saidurga Polamraju <saidurgacsea@gmail.com>
    
    * review comments fixed
    
    Signed-off-by: Venkata Saidurga Polamraju <saidurgacsea@gmail.com>
    
    * review comments fixed
    
    Signed-off-by: Venkata Saidurga Polamraju <saidurgacsea@gmail.com>
    
    ---------
    
    Signed-off-by: Venkata Saidurga Polamraju <saidurgacsea@gmail.com>

--- esignet-integration-api/src/main/java/io/mosip/esignet/api/validator/AuthChallengeFactorFormatValidator.java
+++ esignet-integration-api/src/main/java/io/mosip/esignet/api/validator/AuthChallengeFactorFormatValidator.java
@@ -1,8 +1,10 @@
 package io.mosip.esignet.api.validator;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.mosip.esignet.api.dto.AuthChallenge;
 import io.mosip.esignet.api.util.ErrorConstants;
-import io.mosip.esignet.api.validator.AuthChallengeFactorFormat;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,7 +14,11 @@ import org.springframework.util.StringUtils;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 @Component
 public class AuthChallengeFactorFormatValidator implements ConstraintValidator<AuthChallengeFactorFormat, AuthChallenge> {
@@ -23,6 +29,12 @@ public class AuthChallengeFactorFormatValidator implements ConstraintValidator<A
     
     @Autowired
     private Environment environment;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Value("#{${mosip.esignet.authenticator.default.auth-factor.kba.field-details}}")
+    private List<Map<String, String>> fieldDetailList;
 
     @Override
     public boolean isValid(AuthChallenge authChallenge, ConstraintValidatorContext context) {
@@ -42,7 +54,32 @@ public class AuthChallengeFactorFormatValidator implements ConstraintValidator<A
         int min = environment.getProperty(String.format(MIN_LENGTH_KEY_PREFIX, authFactor), Integer.TYPE, 50);
         int max = environment.getProperty(String.format(MAX_LENGTH_KEY_PREFIX, authFactor), Integer.TYPE, 50);
         String challenge = authChallenge.getChallenge();
-        int length = StringUtils.hasText(challenge)? challenge.length():0 ;
-        return length>=min && length<=max;
+        int length = StringUtils.hasText(challenge) ? challenge.length() : 0;
+        if (!(length >= min && length <= max)) {
+            return false;
+        }
+        if (authFactor.equals("KBA")) {
+            return validateChallenge(authChallenge.getChallenge());
+        }
+        return length >= min && length <= max;
+    }
+
+    private boolean validateChallenge(String challenge) {
+        byte[] decodedBytes = Base64.getDecoder().decode(challenge);
+        String decodedString = new String(decodedBytes, StandardCharsets.UTF_8);
+        Map<String, String> challengeMap;
+        try {
+            challengeMap = objectMapper.readValue(decodedString, new TypeReference<Map<String, String>>() {
+            });
+        } catch (JsonProcessingException e) {
+            return false;
+        }
+        for (Map<String, String> fieldDetail : fieldDetailList) {
+            String id=fieldDetail.get("id");
+            if(fieldDetail.containsKey("regex") && challengeMap.containsKey(id) && !Pattern.compile(fieldDetail.get("regex")).matcher(challengeMap.get(id)).matches()) {
+                return false;
+            }
+        }
+        return true;
     }
 }
