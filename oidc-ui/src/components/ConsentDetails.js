5a99231d75c97b514ebcd5b393023ef557e3e9ce
Gk <76690271+GurukiranP@users.noreply.github.com>
2024-05-22 17:19:03 +0530

    [ES-675] Temporary code change for testing L2 flow. (#713)
    * [ES-675] Temporary code change for testing L2 flow.
    
    Signed-off-by: GurukiranP <talk2gurukiran@gmail.com>
    
    * [ES-675] Temporary code change for testing L2 flow.
    
    Signed-off-by: GurukiranP <talk2gurukiran@gmail.com>
    
    ---------
    
    Signed-off-by: GurukiranP <talk2gurukiran@gmail.com>

--- oidc-ui/src/components/ConsentDetails.js
+++ oidc-ui/src/components/ConsentDetails.js
@@ -55,7 +55,7 @@ const ConsentDetails = ({
       const { response, errors } = consentDetailsResponse;
 
       if (errors != null && errors.length > 0) {
-        redirectOnError(errors[0].errorCode, t2(`${errors[0].errorCode}`));
+        // redirectOnError(errors[0].errorCode, t2(`${errors[0].errorCode}`));
         return;
       } else {
         let claimsScopes = [];
@@ -131,7 +131,7 @@ const ConsentDetails = ({
         setClaimsScopes(claimsScopes);
       }
     } catch (error) {
-      redirectOnError("authorization_failed_msg", error.message);
+      // redirectOnError("authorization_failed_msg", error.message);
     }
   };
 
@@ -141,25 +141,28 @@ const ConsentDetails = ({
 
   const handleProceed = async () => {
     window.onbeforeunload = null;
-    try {
-      const signupRedirectResponse = await authServices.prepareSignupRedirect(
-        transactionId,
-        window.location.href
-      );
-      const { response, errors } = signupRedirectResponse;
-
-      if (errors != null && errors.length > 0) {
-        redirectOnError(errors[0].errorCode, t2(`${errors[0].errorCode}`));
-        return;
-      } else {
-        const encodedIdToken = btoa(response.idToken);
-        window.location.replace(
-          `${eKYCStepsURL}?state=${state}#${encodedIdToken}`
+         window.location.replace(
+          `${eKYCStepsURL}?state=${state}#id_token`
         );
-      }
-    } catch (error) {
-      redirectOnError("authorization_failed_msg", error.message);
-    }
+    // try {
+    //   const signupRedirectResponse = await authServices.prepareSignupRedirect(
+    //     transactionId,
+    //     window.location.href
+    //   );
+    //   const { response, errors } = signupRedirectResponse;
+
+    //   if (errors != null && errors.length > 0) {
+    //     redirectOnError(errors[0].errorCode, t2(`${errors[0].errorCode}`));
+    //     return;
+    //   } else {
+    //     const encodedIdToken = btoa(response.idToken);
+    //     window.location.replace(
+    //       `${eKYCStepsURL}?state=${state}#${encodedIdToken}`
+    //     );
+    //   }
+    // } catch (error) {
+    //   redirectOnError("authorization_failed_msg", error.message);
+    // }
   };
 
   const handleCancel = () => {
