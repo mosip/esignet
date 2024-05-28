e22236d109adcf36d9e13a5be53d5001dc7da354
Gk <76690271+GurukiranP@users.noreply.github.com>
2024-05-28 18:31:55 +0530

    [INJIMOB-1419] Added id attribute. (#726) (#728)
    Signed-off-by: GurukiranP <talk2gurukiran@gmail.com>

--- oidc-ui/src/common/ErrorBanner.js
+++ oidc-ui/src/common/ErrorBanner.js
@@ -19,12 +19,14 @@ const ErrorBanner = ({
         "flex justify-between items-center px-2 sm:px-5 lg:-mx-5 md:-mx-4 sm:-mx-3 -mx-3 error-banner " +
         customClass
       }
+      id="error-banner"
     >
-      <div className="error-banner-text text-sm font-semibold">{t(errorCode)}</div>
+      <div className="error-banner-text text-sm font-semibold" id="error-banner-message">{t(errorCode)}</div>
       <img
         onClick={onCloseHandle}
         className="h-2.5 w-2.5 hover:cursor-pointer"
         src="images/cross_icon.svg"
+        id="error-close-button"
       />
     </div>
   );
