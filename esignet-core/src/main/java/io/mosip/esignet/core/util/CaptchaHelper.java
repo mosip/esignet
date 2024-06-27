205f32e466da2ae5cb2751fd2c2c32e2c5dcbd24
Venkata Saidurga Polamraju <saidurgacsea@gmail.com>
2024-06-27 23:55:11 +0530

    [ES-904] Added test cases
    Signed-off-by: Venkata Saidurga Polamraju <saidurgacsea@gmail.com>

--- esignet-core/src/main/java/io/mosip/esignet/core/util/CaptchaHelper.java
+++ esignet-core/src/main/java/io/mosip/esignet/core/util/CaptchaHelper.java
@@ -34,41 +34,44 @@ public class CaptchaHelper {
 
     public boolean validateCaptcha(String captchaToken) {
 
-        if (captchaToken == null || captchaToken.isEmpty()) {
+        if (captchaToken == null || captchaToken.isBlank()) {
             throw new EsignetException(ErrorConstants.INVALID_CAPTCHA);
         }
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        CaptchaRequest captchaRequest = new CaptchaRequest();
-        captchaRequest.setCaptchaToken(captchaToken);
-        captchaRequest.setModuleName(moduleName);
+        try{
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            CaptchaRequest captchaRequest = new CaptchaRequest();
+            captchaRequest.setCaptchaToken(captchaToken);
+            captchaRequest.setModuleName(moduleName);
 
-        RequestWrapper<CaptchaRequest> requestWrapper = new RequestWrapper<>();
-        requestWrapper.setRequestTime(ZonedDateTime.now(ZoneOffset.UTC).format(DateTimeFormatter.ofPattern(UTC_DATETIME_PATTERN)));
-        requestWrapper.setRequest(captchaRequest);
-
-        RequestEntity<RequestWrapper<CaptchaRequest>> requestEntity = RequestEntity
+            RequestWrapper<CaptchaRequest> requestWrapper = new RequestWrapper<>();
+            requestWrapper.setRequestTime(ZonedDateTime.now(ZoneOffset.UTC).format(DateTimeFormatter.ofPattern(UTC_DATETIME_PATTERN)));
+            requestWrapper.setRequest(captchaRequest);
+            RequestEntity<RequestWrapper<CaptchaRequest>> requestEntity = RequestEntity
                 .post(URI.create(validatorUrl))
                 .headers(headers)
                 .body(requestWrapper);
 
-        ResponseEntity<?> responseEntity = restTemplate.exchange(
-                requestEntity,
-                ResponseEntity.class
-        );
+            ResponseEntity<ResponseWrapper> responseEntity = restTemplate.exchange(
+                    requestEntity,
+                    ResponseWrapper.class
+            );
 
-        if (responseEntity.getStatusCode().is2xxSuccessful() && responseEntity.getBody() != null) {
-            ResponseWrapper<?> responseWrapper = (ResponseWrapper<?>) responseEntity.getBody();
-            if (responseWrapper != null && responseWrapper.getResponse() != null) {
-                log.info("Captcha Validation Successful");
-                return true;
+            if (responseEntity.getStatusCode().is2xxSuccessful() && responseEntity.getBody() != null) {
+                ResponseWrapper<?> responseWrapper = (ResponseWrapper<?>) responseEntity.getBody();
+                if (responseWrapper != null && responseWrapper.getResponse() != null) {
+                    log.info("Captcha Validation Successful");
+                    return true;
+                }
+                log.error("Errors received from CaptchaService: {}", responseWrapper.getErrors()); //NOSONAR responseWrapper is already evaluated to be not null
+                throw new EsignetException(ErrorConstants.INVALID_CAPTCHA);
             }
-            log.error("Errors received from CaptchaService: {}", responseWrapper.getErrors()); //NOSONAR responseWrapper is already evaluated to be not null
+            log.error("Errors received from CaptchaService: {}", responseEntity.getStatusCode());
+            throw new EsignetException(ErrorConstants.INVALID_CAPTCHA);
+        } catch (Exception e) {
             throw new EsignetException(ErrorConstants.INVALID_CAPTCHA);
         }
-        log.error("Errors received from CaptchaService: {}",responseEntity.getStatusCode());
-        throw new EsignetException(ErrorConstants.INVALID_CAPTCHA);
     }
 
 }
