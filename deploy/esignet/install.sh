3eb5f950acf50a4b8fdd18cc660d87d9f4f2b9d9
ckm007 <chandrakeshavmishra@gmail.com>
2024-08-28 17:02:27 +0530

    [MOSIP-32901] added esignet deploy scripts
    Signed-off-by: ckm007 <chandrakeshavmishra@gmail.com>
    Signed-off-by: Chandra Keshav Mishra <chandrakeshavmishra@gmail.com>

---
+++ deploy/esignet/install.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+# Installs all esignet helm charts
+## Usage: ./install.sh [kubeconfig]
+
+if [ $# -ge 1 ] ; then
+  export KUBECONFIG=$1
+fi
+
+NS=esignet
+CHART_VERSION=1.4.0
+
+ESIGNET_HOST=$(kubectl -n esignet get cm esignet-global -o jsonpath={.data.mosip-esignet-host})
+
+echo Create $NS namespace
+kubectl create ns $NS
+
+function installing_esignet() {
+
+  echo Istio label
+  helm repo add mosip https://mosip.github.io/mosip-helm
+  helm repo update
+
+  COPY_UTIL=../copy_cm_func.sh
+  $COPY_UTIL configmap esignet-softhsm-share softhsm $NS
+
+  while true; do
+    read -p "Is Prometheus Service Monitor Operator deployed in the k8s cluster? (y/n): " response
+    if [[ "$response" == "y" || "$response" == "Y" ]]; then
+      servicemonitorflag=true
+      break
+    elif [[ "$response" == "n" || "$response" == "N" ]]; then
+      servicemonitorflag=false
+      break
+    else
+      echo "Not a correct response. Please respond with y (yes) or n (no)."
+    fi
+  done
+
+  echo "Do you have public domain & valid SSL? (Y/n) "
+  echo "Y: if you have public domain & valid ssl certificate"
+  echo "n: If you don't have a public domain and a valid SSL certificate. Note: It is recommended to use this option only in development environments."
+  read -p "" flag
+
+  if [ -z "$flag" ]; then
+    echo "'flag' was provided; EXITING;"
+    exit 1;
+  fi
+  ENABLE_INSECURE=''
+  if [ "$flag" = "n" ]; then
+    ENABLE_INSECURE='--set enable_insecure=true';
+  fi
+
+  echo Installing esignet
+  helm -n $NS install esignet mosip/esignet --version $CHART_VERSION $ENABLE_INSECURE --set metrics.serviceMonitor.enabled=$servicemonitorflag --wait
+
+  kubectl -n $NS  get deploy -o name |  xargs -n1 -t  kubectl -n $NS rollout status
+
+  echo Installed esignet service
+  return 0
+}
+
+# set commands for error handling.
+set -e
+set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
+set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable
+set -o errtrace  # trace ERR through 'time command' and other functions
+set -o pipefail  # trace ERR through pipes
+installing_esignet   # calling function
