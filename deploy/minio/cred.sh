0999dee724faa41aca4f11cbd8dba658edc5c2a2
ckm007 <chandrakeshavmishra@gmail.com>
2024-08-28 17:21:12 +0530

    [MOSIP-32901] added minio deploy scripts
    Signed-off-by: ckm007 <chandrakeshavmishra@gmail.com>
    Signed-off-by: Chandra Keshav Mishra <chandrakeshavmishra@gmail.com>

---
+++ deploy/minio/cred.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+# Creates configmap and secrets for S3/Minio
+# Specific "" for region for minio local installation
+## Usage: ./install.sh [kubeconfig]
+
+if [ $# -ge 3 ] ; then
+  export KUBECONFIG=$4
+fi
+
+NS=s3
+
+echo Create $NS namespace
+kubectl create ns $NS 
+
+function installing_Cred() {
+  echo Istio label
+  kubectl label ns $NS istio-injection=enabled --overwrite
+
+  echo Select the type of object-store to be used:
+  echo 1: For minio native using helm charts
+  echo 2: For any other s3 object store like AWS
+  while read -p "Please choose the correct option as mentioned above(1/2)" choice
+  do
+    if [ $choice = "1" ]
+    then
+    read -p "Please provide pretext value : " PRETEXT_VALUE
+    echo Creating secrets as per Minio native installation
+    USER=$(kubectl -n minio get secret minio -o jsonpath='{.data.root-user}' | base64 --decode)
+    PASS=$(kubectl -n minio get secret minio -o jsonpath='{.data.root-password}' | base64 --decode)
+    kubectl -n s3 create configmap s3 --from-literal=s3-user-key=$USER --from-literal=s3-region="" --dry-run=client  -o yaml | kubectl apply -f -
+    kubectl -n s3 create secret generic s3 --from-literal=s3-user-secret=$PASS --from-literal=s3-pretext-value=$PRETEXT_VALUE --dry-run=client  -o yaml | kubectl apply -f -
+    echo object-store secret and config map is set now.
+    break
+    elif [ $choice = "2" ]
+    then
+    read -p "Enter the S3 user key " USER
+    read -p "Enter the S3 secret key" PASS
+    read -p "Enter the S3 region" REGION
+    read -p "Please provide pretext value : " PRETEXT_VALUE
+    kubectl -n s3 create configmap s3 --from-literal=s3-user-key=$USER --from-literal=s3-region=$REGION --dry-run=client  -o yaml | kubectl apply -f -
+    kubectl -n s3 create secret generic s3 --from-literal=s3-user-secret=$PASS --dry-run=client  -o yaml | kubectl apply -f -
+    kubectl -n s3 create secret generic s3 --from-literal=s3-user-secret=$PASS --from-literal=s3-pretext-value=$PRETEXT_VALUE --dry-run=client  -o yaml | kubectl apply -f -
+    echo object-store secret and config map is set now.
+    break
+    else
+    echo You entered wrong choice
+    kill -9 `ps --pid $$ -oppid=`; exit
+    break
+    fi
+  done
+  return 0
+}
+
+# set commands for error handling.
+set -e
+set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
+set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable
+set -o errtrace  # trace ERR through 'time command' and other functions
+set -o pipefail  # trace ERR through pipes
+installing_Cred   # calling function
---
+++ deploy/minio/install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Installs MinIO using helm chart inside MOSIP cluster
+## Usage: ./install.sh [kubeconfig]
+
+if [ $# -ge 1 ] ; then
+  export KUBECONFIG=$1
+fi
+
+NS=minio
+
+echo Create $NS namespace
+kubectl create ns $NS
+kubectl label ns $NS istio-injection=enabled --overwrite
+
+function installing_minio() {
+  echo Installing minio
+  helm -n minio install minio mosip/minio -f values.yaml --version 10.1.6 --wait
+
+  echo Installing gateways and virtualservice
+  EXTERNAL_HOST=$(kubectl -n esignet get cm esignet-global -o jsonpath={.data.mosip-minio-host})
+
+  echo host: $EXTERNAL_HOST
+  helm -n $NS install istio-addons chart/istio-addons --set externalHost=$EXTERNAL_HOST
+
+  echo Helm installed. Next step is to execute the cred.sh to update secrets in s3 namespace
+  return 0
+}
+
+# set commands for error handling.
+set -e
+set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
+set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable
+set -o errtrace  # trace ERR through 'time command' and other functions
+set -o pipefail  # trace ERR through pipes
+installing_minio   # calling function
