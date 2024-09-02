e21b550f9a2029a6963d05af4abac6ac09fdae52
ckm007 <chandrakeshavmishra@gmail.com>
2024-09-02 16:08:15 +0530

    [MOSIP-32901] updated onboarder esignet install scripts
    Signed-off-by: ckm007 <chandrakeshavmishra@gmail.com>

--- partner-onboarder/install.sh
+++ partner-onboarder/install.sh
@@ -2,103 +2,97 @@
 # Onboards default partners 
 ## Usage: ./install.sh [kubeconfig]
 
-if [ $# -ge 1 ] ; then
+if [ $# -ge 1 ]; then
   export KUBECONFIG=$1
 fi
 
-echo "Do you have public domain & valid SSL? (Y/n) "
-echo "Y: if you have public domain & valid ssl certificate"
+read -p "Is MOSIP used as Identity Provider? (Y/n): " ans
+if [[ "$ans" == "n" || "$ans" == "N" ]]; then
+  echo "Since MOSIP is not the Identity Provider here, Onboarder execution is not required."
+  exit 1
+elif [[ "$ans" != "y" && "$ans" != "Y" ]]; then
+  echo "Please choose between (Y,y) or (N,n)"
+  exit 1
+fi
+
+echo "Do you have a public domain & valid SSL? (Y/n)"
+echo "Y: if you have a public domain & valid SSL certificate"
 echo "n: If you don't have a public domain and a valid SSL certificate. Note: It is recommended to use this option only in development environments."
 read -p "" flag
 
 if [ -z "$flag" ]; then
-  echo "'flag' was provided; EXITING;"
-  exit 1;
+  echo "'flag' was not provided; EXITING;"
+  exit 1
 fi
+
 ENABLE_INSECURE=''
 if [ "$flag" = "n" ]; then
-  ENABLE_INSECURE='--set onboarding.configmaps.onboarding.ENABLE_INSECURE=true';
+  ENABLE_INSECURE='--set onboarding.configmaps.onboarding.ENABLE_INSECURE=true'
 fi
 
 NS=esignet
 CHART_VERSION=0.0.1-develop
 
-echo Create $NS namespace
-kubectl create ns $NS
+echo "Create $NS namespace"
+kubectl create ns $NS || true
 
 function installing_onboarder() {
 
-  read -p "Is values.yaml for onboarder chart set correctly as part of Pre-requisites?(Y/n) " yn;
-  if [ $yn = "Y" ]; then
-    echo Istio label
+  read -p "Is values.yaml for the onboarder chart set correctly as part of prerequisites? (Y/n): " yn
+  if [[ "$yn" == "Y" || "$yn" == "y" ]]; then
+    echo "Istio label"
     kubectl label ns $NS istio-injection=disabled --overwrite
     helm repo update
 
-    echo Copy configmaps and secrets
+    echo "Copy configmaps and secrets"
     kubectl -n $NS --ignore-not-found=true delete cm s3
     kubectl -n $NS delete cm --ignore-not-found=true onboarding
 
-    COPY_UTIL=../deploy//copy_cm_func.sh
+    COPY_UTIL=../deploy/copy_cm_func.sh
     $COPY_UTIL configmap keycloak-env-vars keycloak $NS
     $COPY_UTIL configmap keycloak-host keycloak $NS
 
-    $COPY_UTIL secret s3 s3 $DST_NS
-    $COPY_UTIL secret keycloak keycloak $DST_NS
-    $COPY_UTIL secret keycloak-client-secrets keycloak $DST_NS
+    $COPY_UTIL secret s3 s3 $NS
+    $COPY_UTIL secret keycloak keycloak $NS
+    $COPY_UTIL secret keycloak-client-secrets keycloak $NS
 
-    read -p "Provide onboarder bucket name : " s3_bucket
-    if [[ -z $s3_bucket ]]; then
-      echo "s3_bucket not provided; EXITING;";
-      exit 1;
-    fi
-    if [[ $s3_bucket == *[' !@#$%^&*()+']* ]]; then
-      echo "s3_bucket should not contain spaces / any special character; EXITING";
-      exit 1;
-    fi
-    read -p "Provide onboarder s3 bucket region : " s3_region
-    if [[ $s3_region == *[' !@#$%^&*()+']* ]]; then
-      echo "s3_region should not contain spaces / any special character; EXITING";
-      exit 1;
-    fi
+    s3_url=$(kubectl -n s3 get cm s3 -o json | jq -r '.data."s3-url"')
+    s3_region=$(kubectl -n s3 get cm s3 -o json | jq -r '.data."s3-region"')
+    s3_bucket=$(kubectl -n s3 get cm s3 -o json | jq -r '.data."s3-onboarder-bucket"')
+    s3_user_key=$(kubectl -n s3 get cm s3 -o json | jq -r '.data."s3-user-key"')
 
-    read -p "Provide S3 URL : " s3_url
-    if [[ -z $s3_url ]]; then
-      echo "s3_url not provided; EXITING;"
-      exit 1;
-    fi
+    echo "Onboarding default partners"
+    helm -n $NS install partner-onboarder mosip/partner-onboarder \
+      --set onboarding.configmaps.s3.s3-host="$s3_url" \
+      --set onboarding.configmaps.s3.s3-user-key="$s3_user_key" \
+      --set onboarding.configmaps.s3.s3-region="$s3_region" \
+      --set onboarding.configmaps.s3.s3-bucket-name="$s3_bucket" \
+      --set extraEnvVarsCM[0]=esignet-global \
+      --set extraEnvVarsCM[1]=keycloak-env-vars \
+      --set extraEnvVarsCM[2]=keycloak-host \
+      $ENABLE_INSECURE \
+      -f values.yaml \
+      --version $CHART_VERSION --wait-for-jobs
 
-    s3_user_key=$( kubectl -n s3 get cm s3 -o json | jq -r '.data."s3-user-key"' )
-
-    echo Onboarding default partners
-    helm -n $NS install esignet-resident-oidc-partner-onboarder mosip/partner-onboarder \
-    --set onboarding.configmaps.s3.s3-host="$s3_url" \
-    --set onboarding.configmaps.s3.s3-user-key="$s3_user_key" \
-    --set onboarding.configmaps.s3.s3-region="$s3_region" \
-    --set onboarding.configmaps.s3.s3-bucket-name="$s3_bucket" \
-    $ENABLE_INSECURE \
-    -f values.yaml \
-    --version $CHART_VERSION \
-    --wait --wait-for-jobs
-
-    $COPY_UTIL secret resident-oidc-onboarder-key esignet resident
     kubectl -n $NS set env --keys=mosip-esignet-misp-key --from secret/esignet-misp-onboarder-key deployment/esignet-config-server --prefix=SPRING_CLOUD_CONFIG_SERVER_OVERRIDES_
-    kubectl -n $NS set env --keys=resident-oidc-clientid --from secret/resident-oidc-onboarder-key deployment/esignet-config-server --prefix=SPRING_CLOUD_CONFIG_SERVER_OVERRIDES_
     kubectl -n $NS rollout restart deploy esignet-config-server
     kubectl -n $NS get deploy esignet-config-server -o name | xargs -n1 -t kubectl -n $NS rollout status
     kubectl -n $NS rollout restart deployment esignet
-    kubectl -n resident rollout restart deployment resident
-    echo eSignet MISP License Key and Resident OIDC Client ID updated successfully.
-
-    echo Reports are moved to S3 under onboarder bucket
-
+    echo "eSignet MISP License Key updated successfully."
+    echo "Reports are moved to S3 under $s3_bucket bucket."
     return 0
+  else
+    echo "Please ensure that values.yaml is correctly set."
+    exit 1
   fi
 }
 
-# set commands for error handling.
+# Set commands for error handling.
 set -e
-set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
-set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable
-set -o errtrace  # trace ERR through 'time command' and other functions
-set -o pipefail  # trace ERR through pipes
-installing_onboarder   # calling function
+set -o errexit   ## Exit the script if any statement returns a non-true return value
+set -o nounset   ## Exit the script if you try to use an uninitialized variable
+set -o errtrace  ## Trace ERR through 'time command' and other functions
+set -o pipefail  ## Trace ERR through pipes
+
+# Call the function
+installing_onboarder
