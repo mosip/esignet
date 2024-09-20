124995d681887e6c9ace3f30c3c7ad63c9be8a6d
Mohd Kaif Siddique <mohdkaif.siddique@ad.infosys.com>
2024-09-20 17:43:19 +0530

    modified resumeHaltedTransaction method and end enp-point name from '/resume' to '/complete'
    Signed-off-by: Mohd Kaif Siddique <mohdkaif.siddique@ad.infosys.com>

--- esignet-core/src/main/java/io/mosip/esignet/core/constants/ErrorConstants.java
+++ esignet-core/src/main/java/io/mosip/esignet/core/constants/ErrorConstants.java
@@ -92,4 +92,6 @@ public class ErrorConstants {
     public static final String INVALID_VERIFICATION = "invalid_verification";
     public static final String INVALID_VERIFIED_CLAIMS = "invalid_verified_claims";
     public static final String INVALID_PURPOSE="invalid_purpose";
+
+    public static final String RESUME_NOT_APPLICABLE = "resume_not_applicable";
 }
