ce849828958114e73b4419da5dc0f06ef2d70455
Venkata Saidurga Polamraju <saidurgacsea@gmail.com>
2024-10-01 18:50:58 +0530

    [ES-1148] inserted mock binding service
    Signed-off-by: Venkata Saidurga Polamraju <saidurgacsea@gmail.com>

--- db_upgrade_script/mosip_esignet/sql/1.4.1_to_1.5.0_upgrade.sql
+++ db_upgrade_script/mosip_esignet/sql/1.4.1_to_1.5.0_upgrade.sql
@@ -2,3 +2,4 @@
 
 truncate table consent_detail;
 truncate table consent_history;
+INSERT INTO esignet.KEY_POLICY_DEF(APP_ID,KEY_VALIDITY_DURATION,PRE_EXPIRE_DAYS,ACCESS_ALLOWED,IS_ACTIVE,CR_BY,CR_DTIMES) VALUES('MOCK_BINDING_SERVICE', 1095, 50, 'NA', true, 'mosipadmin', now());
