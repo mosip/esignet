a8e23bceb2228142a1340ae78530386642fd32f0
Venkata Saidurga Polamraju <saidurgacsea@gmail.com>
2024-10-23 10:55:26 +0530

    [ES-1884] Added db upgrade scripts
    Signed-off-by: Venkata Saidurga Polamraju <saidurgacsea@gmail.com>

--- db_upgrade_script/mosip_esignet/sql/1.5.0_to_1.5.1_upgrade.sql
+++ db_upgrade_script/mosip_esignet/sql/1.5.0_to_1.5.1_upgrade.sql
@@ -1,1 +1,8 @@
-\echo 'Upgrade Queries not required for transition from $CURRENT_VERSION to $UPGRADE_VERSION'
+-- Add the new column with a default value
+ALTER TABLE client_detail
+ADD COLUMN additional_config jsonb DEFAULT '{}'::jsonb;
+
+-- Update existing entries to set the default value for the new column
+UPDATE client_detail
+SET additional_config = '{}'::jsonb
+WHERE additional_config IS NULL;
