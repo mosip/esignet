b714b92d0b7351f09da1b3a64417b92aaca977d8
pvsaidurga <132046494+pvsaidurga@users.noreply.github.com>
2024-11-08 16:16:28 +0530

    [ES-1884] Added db upgrade scripts (#972)
    Signed-off-by: Venkata Saidurga Polamraju <saidurgacsea@gmail.com>
    Signed-off-by: pvsaidurga <132046494+pvsaidurga@users.noreply.github.com>

--- db_upgrade_script/mosip_esignet/sql/1.5.0_to_1.5.1_upgrade.sql
+++ db_upgrade_script/mosip_esignet/sql/1.5.0_to_1.5.1_upgrade.sql
@@ -31,6 +31,14 @@ EXCEPTION
 END;
 $$ LANGUAGE plpgsql;
 
+-- Add the new column with a default value
+ALTER TABLE client_detail
+ADD COLUMN additional_config jsonb DEFAULT '{}'::jsonb;
+
+-- Update existing entries to set the default value for the new column
+UPDATE client_detail
+SET additional_config = '{}'::jsonb
+WHERE additional_config IS NULL;
 
 DO $$
 BEGIN
