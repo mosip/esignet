4abe63c4d6dc452313763059e77c62f2f632c3ec
VSIVAKALYAN <kalyanvellanki321@gmail.com>
2024-11-29 10:44:09 +0530

    [MOSIP-37815] Tested Working as expected
    Signed-off-by: VSIVAKALYAN <kalyanvellanki321@gmail.com>

--- deploy/esignet/install.sh
+++ deploy/esignet/install.sh
@@ -87,7 +87,6 @@ echo Installing eSignet with SPRING_KAFKA_CONSUMER_GROUP_ID="esignet-consumer-$N
 
   echo Installing esignet
   helm -n $NS install esignet mosip/esignet --version $CHART_VERSION $ENABLE_INSECURE $plugin_option \
-  --set springkafkaConsumerGroupId="esignet-consumer-$NS" \
   --set metrics.serviceMonitor.enabled=$servicemonitorflag -f values.yaml --wait
 
   kubectl -n $NS  get deploy -o name |  xargs -n1 -t  kubectl -n $NS rollout status
