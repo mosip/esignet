93b44c0a12d98bb62c59afed412bbe49a50fc72f
Sachin Rana <sacrana324@gmail.com>
2025-03-17 22:45:01 +0530

    removed unnecessary do block
    Signed-off-by: Sachin Rana <sacrana324@gmail.com>

--- db_upgrade_script/mosip_esignet/sql/1.5.1_to_1.6.0_rollback.sql
+++ db_upgrade_script/mosip_esignet/sql/1.5.1_to_1.6.0_rollback.sql
@@ -1,8 +1,3 @@
 \c mosip_esignet
 
-DO $$
-BEGIN
-
 ALTER TABLE client_detail DROP COLUMN additional_config;
-
-END $$
--- db_upgrade_script/mosip_esignet/sql/1.5.1_to_1.6.0_upgrade.sql
+++ db_upgrade_script/mosip_esignet/sql/1.5.1_to_1.6.0_upgrade.sql
@@ -1,8 +1,3 @@
 \c mosip_esignet
 
-DO $$
-BEGIN
-
 ALTER TABLE client_detail ADD COLUMN additional_config jsonb;
-
-END $$
