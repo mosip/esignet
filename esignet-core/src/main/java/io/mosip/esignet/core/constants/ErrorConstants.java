d05f42b6686e629df547c4bf68e70aa60c516a1e
pvsaidurga <saidurgacsea@gmail.com>
2025-03-25 14:49:38 +0530

    [MOSIP-34406] Updated rollback script
    Signed-off-by: pvsaidurga <saidurgacsea@gmail.com>

--- esignet-core/src/main/java/io/mosip/esignet/core/constants/ErrorConstants.java
+++ esignet-core/src/main/java/io/mosip/esignet/core/constants/ErrorConstants.java
@@ -13,7 +13,7 @@ public class ErrorConstants {
     public static final String INVALID_CLIENT_NAME="invalid_client_name";
     public static final String INVALID_CLIENT_NAME_MAP_KEY="invalid_language_code";
     public static final String INVALID_CLIENT_NAME_MAP_VALUE="invalid_client_name_value";
-    public static final String INVALID_CLIENT_NAME_MAP_LENGTH="invalid_client_name_map_length";
+    public static final String INVALID_CLIENT_NAME_LENGTH="invalid_client_name_length";
     public static final String INVALID_URI="invalid_uri";
     public static final String INVALID_RP_ID="invalid_rp_id";
     public static final String INVALID_RESPONSE_TYPE="invalid_response_type";
