75f48c6bc56cc19c7af6a2781fe3d38079b1642f
Nandhukumar <nandhukumare@gmail.com>
2025-05-20 16:20:44 +0530

    MOSIP-33883
    Signed-off-by: Nandhukumar <nandhukumare@gmail.com>

--- api-test/src/main/java/io/mosip/testrig/apirig/esignet/utils/EsignetConstants.java
+++ api-test/src/main/java/io/mosip/testrig/apirig/esignet/utils/EsignetConstants.java
@@ -26,6 +26,14 @@ public class EsignetConstants {
 	
 	public static final String MOSIP_ESIGNET_SUPPORTED_CREDENTIAL_SCOPES_LANGUAGE = "mosip.esignet.supported.credential.scopes";
 	
+	public static final String USE_PRE_CONFIGURED_OTP_STRING = "usePreConfiguredOtp";
+	
+	public static final String PRE_CONFIGURED_OTP_STRING = "preconfiguredOtp";
+	
+	public static final String TRUE_STRING = "true";
+	
+	public static final String ALL_ONE_OTP_STRING = "111111";
+	
 	public static final String DSL = "dsl";
 
 }
