fdad736c095d9f5fb4fda9815f89bc2a7c187137
SradhaMohanty5899 <mohantysradha10@gmail.com>
2025-06-09 12:59:15 +0530

    MOSIP-35628 Added CAPTCHA status logging to test report
    Signed-off-by: SradhaMohanty5899 <mohantysradha10@gmail.com>

--- api-test/src/main/java/io/mosip/testrig/apirig/esignet/utils/EsignetUtil.java
+++ api-test/src/main/java/io/mosip/testrig/apirig/esignet/utils/EsignetUtil.java
@@ -271,6 +271,7 @@ public class EsignetUtil extends AdminTestUtil {
 		
 		//When the captcha is enabled we cannot execute the test case as we can not generate the captcha token
 		if (isCaptchaEnabled() == true) {
+			GlobalMethods.reportServerError(GlobalConstants.CAPTCHA_ENABLED, true);
 			throw new SkipException(GlobalConstants.CAPTCHA_ENABLED_MESSAGE);
 		}
 		
