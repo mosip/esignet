8005a90d04d60eb25b150fc898207b00b185327c
Sachin Rana <sacrana324@gmail.com>
2025-06-30 23:09:11 +0530

    [ES-2359] consistent serialization of oAuthDetails (#1317)
    * consistent serialization of oAuthDetails
    
    Signed-off-by: Sachin Rana <sacrana324@gmail.com>
    
    * removed redundant overloaded method
    
    Signed-off-by: Sachin Rana <sacrana324@gmail.com>
    
    * removed unused import
    
    Signed-off-by: Sachin Rana <sacrana324@gmail.com>
    
    * Exception class change
    
    Signed-off-by: Sachin Rana <sacrana324@gmail.com>
    
    * consistent oauth-details hash in oidc-ui
    
    Signed-off-by: Sachin Rana <sacrana324@gmail.com>
    
    * correcting imports and test cases
    
    Signed-off-by: Sachin Rana <sacrana324@gmail.com>
    
    * review comments
    
    Signed-off-by: Sachin Rana <sacrana324@gmail.com>
    
    ---------
    
    Signed-off-by: Sachin Rana <sacrana324@gmail.com>

--- oidc-ui/src/helpers/utils.js
+++ oidc-ui/src/helpers/utils.js
@@ -24,6 +24,20 @@ const checkConfigProperty = (config, property) => {
   return false;
 };
 
+const sortKeysDeep = obj => {
+    if (Array.isArray(obj)) {
+      return obj.map(sortKeysDeep);
+    } else if (obj !== null && typeof obj === "object") {
+      return Object.keys(obj)
+        .sort()
+        .reduce((result, key) => {
+          result[key] = sortKeysDeep(obj[key]);
+          return result;
+        }, {});
+    }
+    return obj;
+};
+
 /**
  * Generates a base64url-encoded SHA-256 hash of the given value.
  *
@@ -31,12 +45,12 @@ const checkConfigProperty = (config, property) => {
  * @returns {Promise<string>} A Promise that resolves to the base64url-encoded hash string.
  */
 const getOauthDetailsHash = async (value) => {
-  let sha256Hash = sha256(JSON.stringify(value));
-  let hashB64 = Base64.stringify(sha256Hash)
-    .split("=")[0]
-    .replace(/\+/g, "-")
-    .replace(/\//g, "_");
-  return hashB64;
+    let sha256Hash = sha256(JSON.stringify(sortKeysDeep(value)));
+    let hashB64 = Base64.stringify(sha256Hash)
+        .split("=")[0]
+        .replace(/\+/g, "-")
+        .replace(/\//g, "_");
+    return hashB64;
 };
 
 /**
@@ -45,7 +59,6 @@ const getOauthDetailsHash = async (value) => {
  * @param {string} str - The base64url-encoded string (e.g., a JWT part).
  * @returns {string} The decoded UTF-8 string.
  */
-
 const base64UrlDecode = (str) => {
   return decodeURIComponent(
     decodeHash(str.replace(/-/g, "+").replace(/_/g, "/"))
@@ -55,4 +68,4 @@ const base64UrlDecode = (str) => {
   );
 };
 
-export { encodeString, decodeHash, checkConfigProperty, getOauthDetailsHash, base64UrlDecode };
+export { encodeString, decodeHash, checkConfigProperty, sortKeysDeep, getOauthDetailsHash, base64UrlDecode };
