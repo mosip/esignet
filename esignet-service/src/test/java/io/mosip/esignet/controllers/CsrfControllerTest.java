4202c961debd78ffe3fe349ae40daad24f92d25d
pvsaidurga <saidurgacsea@gmail.com>
2025-10-29 12:47:03 +0530

    [MOSIP-43441] Added session timeout for csrf token
    Signed-off-by: pvsaidurga <saidurgacsea@gmail.com>

--- esignet-service/src/test/java/io/mosip/esignet/controllers/CsrfControllerTest.java
+++ esignet-service/src/test/java/io/mosip/esignet/controllers/CsrfControllerTest.java
@@ -4,12 +4,14 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import io.mosip.esignet.core.config.LocalAuthenticationEntryPoint;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.security.web.csrf.DefaultCsrfToken;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -19,6 +21,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.mosip.esignet.services.CacheUtilService;
+import org.springframework.test.web.servlet.MvcResult;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(value = CsrfController.class)
@@ -41,5 +44,23 @@ public class CsrfControllerTest {
 		mockMvc.perform(get("/csrf/token").content(objectMapper.writeValueAsString(csrfToken))
 				.contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
 	}
+
+    @Test
+    public void getCsrfToken_AfterSessionExpiry_thenFail() throws Exception {
+        MockHttpSession session = new MockHttpSession();
+        CsrfToken oldToken = new DefaultCsrfToken("X-XSRF-TOKEN", "_csrf", "old-token");
+        session.setAttribute(CsrfToken.class.getName(), oldToken);
+        MvcResult result1 = mockMvc.perform(get("/csrf/token").session(session))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String responseBody1 = result1.getResponse().getContentAsString();
+        session.invalidate();
+        MvcResult result2 = mockMvc.perform(get("/csrf/token"))
+                .andExpect(status().isOk())
+                .andReturn();
+        String responseBody2 = result2.getResponse().getContentAsString();
+        Assert.assertNotEquals(responseBody1, responseBody2, "CSRF token should be regenerated after session expiry");
+    }
 
 }
