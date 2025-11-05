94818712cef47bb6cc0ff8f6a980cd9d89344f6f
pvsaidurga <saidurgacsea@gmail.com>
2025-11-05 21:40:30 +0530

    [ES-2620] Updated KBI helper service (#1514)
    Signed-off-by: pvsaidurga <saidurgacsea@gmail.com>

--- esignet-integration-api/src/main/java/io/mosip/esignet/api/util/KBIFormHelperService.java
+++ esignet-integration-api/src/main/java/io/mosip/esignet/api/util/KBIFormHelperService.java
@@ -101,6 +101,7 @@ public class KBIFormHelperService {
                 String fieldId = field.get("id");
                 String type = field.get("type");
                 String regex = field.get("regex");
+                String format = field.getOrDefault("format","yyyy-MM-dd");
 
                 if (fieldId == null || fieldId.trim().isEmpty()) {
                     log.error("Field Id is missing or empty: {}", field);
@@ -123,6 +124,7 @@ public class KBIFormHelperService {
 
                 if ("date".equalsIgnoreCase(type)) {
                     fieldNode.put("type", "date");
+                    fieldNode.put("format", format);
                 }
 
                 schemaArray.add(fieldNode);
@@ -132,15 +134,9 @@ public class KBIFormHelperService {
             finalSchema.set("schema", schemaArray);
             ObjectNode languageNode = objectMapper.createObjectNode();
             languageNode.putArray("mandatory").add("eng");
-            languageNode.putArray("optional").add("khm");
 
             ObjectNode langCodeMap = objectMapper.createObjectNode();
             langCodeMap.put("eng", "en");
-            langCodeMap.put("hin", "hi");
-            langCodeMap.put("ara", "ar");
-            langCodeMap.put("kan", "kn");
-            langCodeMap.put("tam", "ta");
-            langCodeMap.put("khm", "km");
 
             languageNode.set("langCodeMap", langCodeMap);
             finalSchema.set("language", languageNode);
