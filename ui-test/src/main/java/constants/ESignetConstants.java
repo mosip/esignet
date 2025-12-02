4f70d52a542a6006198f242c357c2e570ef7d2ba
Abhishek S <127825992+abhishek8shankar@users.noreply.github.com>
2025-12-02 13:18:45 +0530

    [MOSIP-43898] Removed uitest from esignet (#1558)
    Signed-off-by: Abhi <abhishek.shankarcs@gmail.com>

--- ui-test/src/main/java/constants/ESignetConstants.java
+++
@@ -1,6 +0,0 @@
-package constants;
-
-public class ESignetConstants {
-    public static final String mandatoryLanguageKey = "mosip.signup.idrepo.mandatory-language";
-    public static final String optionalLanguageKey = "mosip.signup.idrepo.optional-language";
-}
--- ui-test/src/main/java/constants/UiConstants.java
+++
@@ -1,5 +0,0 @@
-package constants;
-
-public class UiConstants {
-
-}
