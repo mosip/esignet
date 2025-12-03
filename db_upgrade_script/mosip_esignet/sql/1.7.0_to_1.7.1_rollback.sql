857f5820c675e102b699fbad59bff013e991bcf4
Sachin Rana <sacrana324@gmail.com>
2025-12-03 15:44:17 +0530

    updating project version to 1.8.0 (#1561)
    * updating project version to 1.8.0
    
    Signed-off-by: Sachin Rana <sacrana324@gmail.com>
    
    * corrected license header text
    
    Signed-off-by: Sachin Rana <sacrana324@gmail.com>
    
    ---------
    
    Signed-off-by: Sachin Rana <sacrana324@gmail.com>

--- db_upgrade_script/mosip_esignet/sql/1.7.0_to_1.7.1_rollback.sql
+++ db_upgrade_script/mosip_esignet/sql/1.7.0_to_1.7.1_rollback.sql
@@ -1,7 +1,1 @@
--- This Source Code Form is subject to the terms of the Mozilla PublicAdd commentMore actions
--- License, v. 2.0. If a copy of the MPL was not distributed with this
--- file, You can obtain one at https://mozilla.org/MPL/2.0/.
-
-\c mosip_esignet
-
-DROP TABLE IF EXISTS ca_cert_store;
+\echo 'Rollback Queries not required for transition from $CURRENT_VERSION to $UPGRADE_VERSION'
