9c1c7168fa1b63b898803c39c067ca8168f2c1f6
Sachin Rana <sacrana324@gmail.com>
2026-01-16 16:53:24 +0530

    [ES-2777] Always show configuration through actuator/env with some key/values sanitized (#1608)
    * Always show configuration through actuator/env with some key/values sanitized
    
    Signed-off-by: Sachin Rana <sacrana324@gmail.com>
    
    * license text
    
    Signed-off-by: Sachin Rana <sacrana324@gmail.com>
    
    ---------
    
    Signed-off-by: Sachin Rana <sacrana324@gmail.com>

---
+++ esignet-core/src/test/java/io/mosip/esignet/core/config/ActuatorSanitizationConfigTest.java
@@ -0,0 +1,71 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+package io.mosip.esignet.core.config;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.actuate.endpoint.SanitizableData;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.mockito.Mockito.*;
+
+class ActuatorSanitizationConfigTest {
+
+    private ActuatorSanitizationConfig sanitizingFunction;
+
+    @BeforeEach
+    void setUp() {
+        sanitizingFunction = new ActuatorSanitizationConfig();
+        ReflectionTestUtils.setField(sanitizingFunction, "keysToBeSanitizedEndsWith", List.of("password", "secret"));
+        ReflectionTestUtils.setField(sanitizingFunction, "keysToBeSanitizedContains", List.of("credentials"));
+    }
+
+    @Test
+    void test_sanitizingFunction_endMatch_thenSanitized() {
+        SanitizableData data = mock(SanitizableData.class);
+        SanitizableData sanitizedData = mock(SanitizableData.class);
+
+        when(data.getKey()).thenReturn("db.password");
+        when(data.withSanitizedValue()).thenReturn(sanitizedData);
+
+        SanitizableData result = sanitizingFunction.apply(data);
+
+        assertSame(sanitizedData, result);
+        verify(data).withSanitizedValue();
+        verify(data, atLeastOnce()).getKey();
+    }
+
+    @Test
+    void test_sanitizingFunction_containsMatch_thenSanitized() {
+        SanitizableData data = mock(SanitizableData.class);
+        SanitizableData sanitizedData = mock(SanitizableData.class);
+
+        when(data.getKey()).thenReturn("API_CREDENTIALS_ID");
+        when(data.withSanitizedValue()).thenReturn(sanitizedData);
+
+        SanitizableData result = sanitizingFunction.apply(data);
+
+        assertSame(sanitizedData, result);
+        verify(data).withSanitizedValue();
+        verify(data, atLeastOnce()).getKey();
+    }
+
+    @Test
+    void test_sanitizingFunction_noMatch_thenOriginalData() {
+        SanitizableData data = mock(SanitizableData.class);
+
+        when(data.getKey()).thenReturn("application.name");
+
+        SanitizableData result = sanitizingFunction.apply(data);
+
+        assertSame(data, result);
+        verify(data, never()).withSanitizedValue();
+        verify(data, atLeastOnce()).getKey();
+    }
+}
