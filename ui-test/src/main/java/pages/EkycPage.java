f37b99b6a55b635d47318480011bffbb940e8bb6
Rachana S P <rachana.p@cyberpwn.com>
2026-03-17 10:37:21 +0530

    MOSIP-44540:Resloved the comment
    Signed-off-by: Rachana S P <rachana.p@cyberpwn.com>

--- ui-test/src/main/java/pages/EkycPage.java
+++ ui-test/src/main/java/pages/EkycPage.java
@@ -1,21 +1,11 @@
 package pages;
 
-import java.time.Duration;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import base.BasePage;
-import utils.ClaimsUtil;
 
 public class EkycPage extends BasePage {
 
