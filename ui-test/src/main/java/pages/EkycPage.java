62cf76309b839445e3aeb619c1e1b7bde8893208
Bhuvanashree B S <bhuvanashree.b@cyberpwn.com>
2026-03-23 16:20:37 +0530

    fixed the error that was causing build failure
    Signed-off-by: Bhuvanashree B S <bhuvanashree.b@cyberpwn.com>

--- ui-test/src/main/java/pages/EkycPage.java
+++ ui-test/src/main/java/pages/EkycPage.java
@@ -30,22 +30,22 @@ public class EkycPage extends BasePage {
 	WebElement eKycStep2Subtitle;
 
 	public boolean isEkycProcessStepsScreenLabelDisplayed() {
-		return isElementVisible(ekycProcessStepsScreenLabel);
+		return isElementVisible(ekycProcessStepsScreenLabel, "Verified eKyc Screen is displayed");
 	}
 
 	public boolean isEkycStep1TitleChooseEkycProviderDisplayed() {
-		return isElementVisible(eKycStep1Title);
+		return isElementVisible(eKycStep1Title, "Verified the title step 1 in eKyc screen");
 	}
 
 	public boolean isEkycStep1SubtitleDisplayed() {
-		return isElementVisible(eKycStep1Subtitle);
+		return isElementVisible(eKycStep1Subtitle, "Verified the sub-title step 1 in eKyc screen");
 	}
 
 	public boolean isEkycStep2TitleTermsAndConditionsDisplayed() {
-		return isElementVisible(eKycStep2Title);
+		return isElementVisible(eKycStep2Title, "Verified the title step 2 in eKyc screen");
 	}
 
 	public boolean isEkycStep2SubtitleDisplayed() {
-		return isElementVisible(eKycStep2Subtitle);
+		return isElementVisible(eKycStep2Subtitle, "Verified the sub-title step 2 in ekyc screen");
 	}
 }
