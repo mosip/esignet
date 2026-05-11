e453bb3f526a80bc321bc4296cff3de618949d21
anushasunkada <anushasunkada@gmail.com>
2026-05-11 08:16:26 +0530

    initial commit
    Signed-off-by: anushasunkada <anushasunkada@gmail.com>

--- oidc-ui/public/theme-config.js
+++
@@ -1,8 +0,0 @@
-document
-  .getElementsByTagName("body")[0]
-  .classList.add(window._env_.DEFAULT_THEME);
-document.getElementById("icon").href = window._env_.DEFAULT_FEVICON;
-document.getElementById("title").innerHTML = window._env_.DEFAULT_TITLE;
-if (window._env_.DEFAULT_FONT_URL !== "") {
-  document.getElementById("fonts-url").href = window._env_.DEFAULT_FONT_URL;
-}
