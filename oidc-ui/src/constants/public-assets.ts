dcec9299731f389ab5ae8b240b4ed7a0d5332bd4
Zeeshan Mehboob <zeeshan.mehboob@infosys.com>
2026-05-14 09:47:44 +0530

    [REMOVED] unnecessary images
    Signed-off-by: Zeeshan Mehboob <zeeshan.mehboob@infosys.com>

--- oidc-ui/src/constants/public-assets.ts
+++ oidc-ui/src/constants/public-assets.ts
@@ -8,14 +8,7 @@ export const IMAGES = {
   SECTION_BG: `${BASE_URL}images/section-bg.png`,
   TOP_LEFT_BG_LOGO: `${BASE_URL}images/top_left_bg_logo.svg`,
   BOTTOM_RIGHT_BG_LOGO: `${BASE_URL}images/bottom_right_bg_logo.svg`,
-  CROSS_ICON: `${BASE_URL}images/cross_icon.svg`,
-  ERROR_ICON: `${BASE_URL}images/error_icon.svg`,
-  INFO_ICON: `${BASE_URL}images/info_icon.svg`,
-  WARNING_MESSAGE_ICON: `${BASE_URL}images/warning_message_icon.svg`,
   UNDER_CONSTRUCTION: `${BASE_URL}images/under_construction.svg`,
-  NO_INTERNET: `${BASE_URL}images/no_internet.svg`,
-  LANGUAGE_ICON: `${BASE_URL}images/language_icon.png`,
-  REFRESH_LOGO: `${BASE_URL}images/refresh_logo.png`,
 } as const;
 
 export const CSS_IMAGE_VARIABLES: Record<string, string> = {
@@ -32,6 +25,4 @@ export const CSS_IMAGE_VARIABLES: Record<string, string> = {
 
 export const CONFIG_URLS = {
   THEME_CONFIG: `${BASE_URL}theme/config.json`,
-  LOCALE_CONFIG: `${BASE_URL}locales/default.json`,
-  ENG_CONFIG: `${BASE_URL}locales/en.json`,
 } as const;
