6c9eb21b9144e87fd5b847f86d4b88d100952f13
Sachin Rana <sacrana324@gmail.com>
2026-05-29 19:57:36 +0530

    added dockerfile for oidc-ui
    Signed-off-by: Sachin Rana <sacrana324@gmail.com>

--- oidc-ui/src/constants/routes.ts
+++ oidc-ui/src/constants/routes.ts
@@ -4,6 +4,6 @@ export const CSRF = '/csrf/token';
 // Route endpoints
 export const SOMETHING_WENT_WRONG = '/something-went-wrong';
 export const PAGE_NOT_FOUND = '/page-not-found';
-export const LOGIN = '/login/signin';
+export const LOGIN = '/login';
 export const ESIGNET_DETAIL = '/';
 export const NETWORK_ERROR = '/network-error';
