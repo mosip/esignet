95de4d1de3c2afc36f5185d91c9c1d2907c823a6
Zeeshan Mehboob <zeeshan.mehboob@infosys.com>
2026-06-10 13:00:18 +0530

    [1842] config changes for deployment
    Signed-off-by: Zeeshan Mehboob <zeeshan.mehboob@infosys.com>

--- oidc-ui/src/constants/routes.ts
+++ oidc-ui/src/constants/routes.ts
@@ -1,9 +1,9 @@
 // Authorization endpoints
-export const CSRF = '/csrf/token';
+export const CSRF = "/csrf/token";
 
 // Route endpoints
-export const SOMETHING_WENT_WRONG = '/something-went-wrong';
-export const PAGE_NOT_FOUND = '/page-not-found';
-export const LOGIN = '/login';
-export const ESIGNET_DETAIL = '/';
-export const NETWORK_ERROR = '/network-error';
+export const SOMETHING_WENT_WRONG = "/something-went-wrong";
+export const PAGE_NOT_FOUND = "/page-not-found";
+export const LOGIN = "/signin";
+export const ESIGNET_DETAIL = "/";
+export const NETWORK_ERROR = "/network-error";
