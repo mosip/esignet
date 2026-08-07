d358b1411cdd3d77bc7587d9a26494f58a405da3
anushasunkada <anushasunkada@gmail.com>
2026-08-07 17:32:00 +0530

    Updated docker file with hsm client configurations
    Signed-off-by: anushasunkada <anushasunkada@gmail.com>

--- esignet-service/docker-entrypoint.sh
+++ esignet-service/docker-entrypoint.sh
@@ -1,13 +1,41 @@
 #!/bin/sh
+
+#installs the pkcs11 libraries.
 set -e
-signing_key="${SIGNING_KEY_PATH:-${THUNDERID_SIGNING_KEY:-${DATA_DIR:-./data}/keys/signing.key}}"
-if [ ! -f "${signing_key}" ]; then
-  cert_path="$(dirname "${signing_key}")/signing.crt"
-  mkdir -p "$(dirname "${signing_key}")"
-  openssl req -x509 -newkey rsa:2048 \
-    -keyout "${signing_key}" \
-    -out "${cert_path}" \
-    -days 3650 -nodes -subj "/CN=esignet"
+
+# Check if $hsm_client_zip_url_env is not empty
+if [[ -n "$hsm_client_zip_url_env" ]]; then
+    echo "Download the client from $hsm_client_zip_url_env"
+    wget -q --show-progress "$hsm_client_zip_url_env" -O client.zip
+    echo "Downloaded $hsm_client_zip_url_env"
 fi
-export SIGNING_KEY_PATH="${signing_key}"
+
+DIR_NAME=$hsm_local_dir_env
+FILE_NAME="client.zip"
+
+if [[ -n "$hsm_client_zip_url_env" ]]; then
+  has_parent=$(zipinfo -1 "$FILE_NAME" | awk '{split($NF,a,"/");print a[1]}' | sort -u | wc -l)
+  if test "$has_parent" -eq 1; then
+    echo "Zip has a parent directory inside"
+    dirname=$(zipinfo -1 "$FILE_NAME" | awk '{split($NF,a,"/");print a[1]}' | sort -u | head -n 1)
+    echo "Unzip directory"
+    unzip $FILE_NAME
+    echo "Renaming directory"
+    mv -v $dirname $DIR_NAME
+  else
+    echo "Zip has no parent directory inside"
+    echo "Creating destination directory"
+    mkdir "$DIR_NAME"
+    echo "Unzip to destination directory"
+    unzip -d "$DIR_NAME" $FILE_NAME
+  fi
+
+  echo "Attempting to install"
+  cd ./$DIR_NAME && chmod +x install.sh && sudo ./install.sh
+  echo "Installation complete"
+else
+  echo "*** HSM Client installation is ignored ***"
+fi
+
+cd $work_dir
 exec "$@"
